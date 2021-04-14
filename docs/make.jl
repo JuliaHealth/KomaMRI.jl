612558cfd539bf6de5aa654b718ff67206a84b6c
Carlos Castillo Passi <cncastillo@uc.cl>
2021-04-14 15:28:43 -0400

    Deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,9 @@ using Documenter, MRIsim
 
 makedocs(sitename="MRIsim.jl")
 
+deploydocs(
+    repo = "github.com/cncastillo/MRIsim.jl.git",
+)
+
+#makedocs(format = LaTeX())
 #julia --color=yes make.jl
