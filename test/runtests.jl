a21a28763c5d8e060e58c9fb6ab0e25985c67660
Carlos Castillo Passi <cncastillo@uc.cl>
2022-03-29 23:09:19 -0300

    Testing behaviour of vcat in Julia 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,6 +35,7 @@ end
 @testset "Sequence" begin
     # Rotation 2D case
     A1, A2, T, t = rand(4)
+    println([Grad(A1,T);Grad(A2,T);;])
     s = Sequence([Grad(A1,T);
                   Grad(A2,T);;])
     θ = π*t
@@ -59,11 +60,11 @@ end
     A1, A2, A3, T1 = rand(4)
     s1 = Sequence([Grad(A1,T1);
                    Grad(A2,T1);;],
-                    [RF(A3,T1)])
+                    [RF(A3,T1);;])
     B1, B2, B3, T2 = rand(4)
     s2 = Sequence([Grad(B1,T2);
                    Grad(B2,T2);;],
-                    [RF(B3,T2)])
+                    [RF(B3,T2);;])
     s = s1 + s2
     @test s.GR.A ≈ [s1.GR.A s2.GR.A]
     @test s.RF.A ≈ [s1.RF.A s2.RF.A]
