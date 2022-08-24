728b4e2ae5502e26f9313dcc1284aba40064b543
beorostica-asusFX505GT-win <beorostica@uc.cl>
2022-08-24 16:42:57 -0400

    Detailed docstring for Magnetization.jl

--- src/datatypes/simulation/Magnetization.jl
+++ src/datatypes/simulation/Magnetization.jl
@@ -1,23 +1,84 @@
 """
-    Mag
+    Mag(xy::Complex, z::Real)
 
 The Magnetization object.
+
+# Arguments
+- `xy::Complex`: the magnetization (of a phantom) in the xy plane
+- `z::Real`: the magnetization (of a phantom) in the z plane
 """
 mutable struct Mag
     xy::Complex
     z::Real
 end
-Base.show(io::IO,M::Mag) = print(io, "Mag(xy = ",round(M.xy,digits=2),", z = ",round(M.z,digits=2),")")
-# Contructor
-Mag(p::Phantom, dir::Symbol) = dir==:x ? Mag.(p.ρ,0) : Mag.(0,p.ρ)
+
+"""
+    Mag(p::Phantom, dir::Symbol)
+
+Generates a Vector of Magnetization objects with the information of the `p` panthom object
+in the axis given by the `dir` symbol.
+
+# Arguments
+- `p::Phantom`: the phantom object
+- `dir::Symbol`: custom options are `:x` or `:z`. It is the symbol that represents the axis
+    of the magnetization
+
+# Examples
+```julia-repl
+julia> obj = Phantom(x=zeros(5));
+
+julia> mag = Mag(obj, :z)
+5-element Vector{Mag}:
+ Mag(xy = 0.0 + 0.0im, z = 1.0)
+ Mag(xy = 0.0 + 0.0im, z = 1.0)
+ Mag(xy = 0.0 + 0.0im, z = 1.0)
+ Mag(xy = 0.0 + 0.0im, z = 1.0)
+ Mag(xy = 0.0 + 0.0im, z = 1.0)
+```
+"""
+Mag(p::Phantom, dir::Symbol) = dir==:x ? Mag.(p.ρ, 0) : Mag.(0, p.ρ)
+
+"""
+    str = show(io::IO, x::Mag)
+
+Displays information about the Mag object `x` in the julia REPL.
+
+# Arguments
+- `x::Mag`: the Mag object
+
+# Returns
+- `str` (::String) the output string message
+"""
+Base.show(io::IO, M::Mag) = begin
+    print(io, "Mag(xy = ", round(M.xy, digits=2), ", z = ", round(M.z, digits=2), ")")
+end
+
+"""
+    y = getproperty(x::Vector{Mag}, f::Symbol)
+
+Overchages Base.getproperty(). It is meant to access properties of the Mag vector `x`
+directly without needing to iterate elementwise.
+
+# Arguments
+- `x::Vector{Mag}`: the vector of Mag objects
+- `f::Symbol`: the symbol that represents a property of a Mag object
+
+# Returns
+- `y`: (::Vector{Any}) the vector with the property defined by the `f` symbol for all
+    elements of the Mag vector `x`
+"""
+getproperty(x::Vector{Mag}, f::Symbol) = getproperty.(x, f)
+
+
 # Arithmetic operations
 +(M1::Mag, M2::Mag) = Mag(M1.xy + M2.xy, M1.z + M2.z) #Vector sum
 *(α::Float64, M::Mag) = Mag(α*M.xy, α*M.z) #This was defined to easily define the average M_mean = sum 1/N* M
 *(M::Mag, α::Float64) = Mag(α*M.xy, α*M.z)
+
 # Other operations
 angle(M::Mag) = angle(M.xy)
 abs(M::Mag) = abs(M.xy)
-getproperty(x::Vector{Mag}, f::Symbol) = getproperty.(x,f)
+
 # Rotation
 @doc raw"""
 Spinor (\alpha, \beta) × Magnetization (Mx + i My, Mz)
@@ -47,6 +108,7 @@ Mz+ = (|α|² - |β|²)Mz-α⋆ β⋆ Mxy-αβMxy⋆ .
         (abs(s.α)^2-abs(s.β)^2)*M.z-2real(s.α*s.β*conj(M.xy))#2real(s.α*s.β*conj(M.xy))=conj(s.α)*conj(s.β)*M.xy+s.α*s.β*conj(M.xy)
         )
 end
+
 #Operation on vector
 # M0 = Mag(0,1)
 # Mf = Ry(π/2)*M0
