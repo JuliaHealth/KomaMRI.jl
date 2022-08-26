d122f747b44752f2bf3049170569bb45415f65ea
beorostica-asusFX505GT-win <beorostica@uc.cl>
2022-08-26 14:06:44 -0400

    Revision docstring for TimeStepCalculation.jl

--- src/simulation/TimeStepCalculation.jl
+++ src/simulation/TimeStepCalculation.jl
@@ -5,22 +5,25 @@ Divides a list of indices 1:`N` (which is in your imagination) into `k` groups.
 
 !!! note
     It is possible to predifine some break points at specific indices with the `breaks`
-    keyword, in this case the number of groups could increase.
+    keyword, in this case the number of groups could increase. This is useful to define
+    start and end indices of RF pulses to separate the simulation into excitation and
+    preccesion computations.
 
 # Arguments
-- `N`: (::Int64) number of elements to be ordered (of an imaginary array 1:`N`)
-- `k`: (::Int64) number of groups to divide the `N` elements
+- `N`: (`::Int64`) the number of elements to be ordered (of an imaginary array 1:`N`)
+- `k`: (`::Int64`) the number of groups to divide the `N` elements
 
 # Keywords
-- `type`: (::String) can be "random" or "ordered". If random, then the indices of the groups
-    are unordered. If "ordered", the indices of the groups are sorted in an incremental
-    order
-- `breaks`: (::Vector{Int64}) array of indices where predefined break points are placed
+- `type`: (`::String`, `="random"`, opts: ["random", "ordered"]) the order type option.
+    If random, then the indices of the groups are unordered. If "ordered", then the indices
+    of the groups are sorted in an incremental order
+- `breaks`: (`::Vector{Int64}`, `=[]`) the array of indices where predefined break points
+    are placed
 
 # Returns
-- `array_of_ranges`: (Vector{UnitRange{Int64}}) an array that contains ranges of different
-    groups (the aim target are `k` groups, but this could be increased by adding elements in
-    the `breaks` input array)
+- `array_of_ranges`: (`::Vector{UnitRange{Int64}}`) the array that contains ranges of
+    different groups (the aim target are `k` groups, but this could be increased by adding
+    elements in the `breaks` input array)
 
 # Examples
 ``` julia-repl
@@ -76,24 +79,25 @@ end
     t = points_from_key_times(times; dt)
 
 Returns a vector which contains the same points as `times` but with additional points that
-have a separation of at least `dt`.
+have a separation of at most `dt`.
 
 !!! note
     The last time points could not be present in the output in some cases.
     Some time points could be duplicated in the output.
     Duplicated time points should be removed afterwards (done by
         [`get_variable_times`](@ref)).
-    The output represents time points that cannot be missed out during the simulation.
+    The output represents all time points regarded during the simulation with a "nominal"
+    `dt` separation between two samples.
 
 # Arguments
-- `times`: (::Vector{Float64}) input time array with key points you want to keep
+- `times`: (`::Vector{Float64}`, `[s]`) the time array with key points you want to keep
 
 # Keywords
-- `dt`: (::Float64) maximum delta time separation between two time samples
+- `dt`: (`::Float64`, `[s]`) the maximum delta time separation between two time samples
 
 # Returns
-- `t`: (::Vector{Float64}) output time array with the same points as the input array but
-    with additional points that have a separation of at least `dt`.
+- `t`: (`::Vector{Float64}`, `[s]`) the time array with the same points as the input array but with
+    additional points that have a separation of at most `dt`.
 
 # Examples
 ```julia-repl
@@ -131,7 +135,7 @@ function points_from_key_times(times; dt)
 end
 
 """
-    (t, Δt) = get_uniform_times(seq, Δt; Δt_rf=1e-4)
+    t, Δt = get_uniform_times(seq, Δt; Δt_rf=1e-4)
 
 This function, despite its name, actually gets non-uniform time points. Refer to
 [`get_variable_times`](@ref) for more details.
@@ -142,53 +146,55 @@ This function, despite its name, actually gets non-uniform time points. Refer to
     is bypassed by [`get_variable_times`](@ref).
 
 # Arguments
-- `seq`: (::Sequence) the sequence object
-- `Δt`: (::Real) the nominal delta time separation between two time samples for ADC
+- `seq`: (`::Sequence`) the sequence struct
+- `Δt`: (`::Real`, `[s]`) the nominal delta time separation between two time samples for ADC
     acquisition and Gradients (by nominal we mean that the time separation should be at
-    least `Δt` when the samples are regarded by [`KomaMRI.is_ADC_on`](@ref) or
+    most `Δt` when the samples are regarded by [`KomaMRI.is_ADC_on`](@ref) or
     [`KomaMRI.is_GR_on`](@ref)), otherwise the time points are not necessary and the
     separation will be bigger)
 
 # Keywords
-- `Δt_rf`: (::Real) the nominal delta time separation between two time samples for RF
-    excitation (by nominal we mean that the time separation should be at least `Δt_rf` when
-    the samples are regarded by [`KomaMRI.is_RF_on`](@ref), otherwise the time points are
-    not necessary and the separation will be bigger)
+- `Δt_rf`: (`::Real`, `=1e-4`, `[s]`) the nominal delta time separation between two time
+    samples for RF excitation (by nominal we mean that the time separation should be at most
+    `Δt_rf` when the samples are regarded by [`KomaMRI.is_RF_on`](@ref), otherwise the time
+    points are not necessary and the separation will be bigger)
 
 # Returns
-- `t`: (::Vector{Float64}) the time array with the non-uniform time values
-- `Δt`: (::Vector{Float64}) the delta time array with the separation between two adjacent
-    time points of the `t` array
+- `t`: (`::Vector{Float64}`, `[s]`) the time array with non-uniform time values
+- `Δt`: (`::Vector{Float64}`, `[s]`) the delta time array with the separation between two
+    adjacent time points of the `t` time array
 """
 function get_uniform_times(seq, Δt; Δt_rf=1e-4)
 	t, Δt = get_variable_times(seq; dt=Δt, dt_rf=Δt_rf)
 end
 
 """
-    (t, Δt) = get_variable_times(seq; dt=1, dt_rf=1e-4)
+    t, Δt = get_variable_times(seq; dt=1, dt_rf=1e-4)
 
-This function returns non-uniform time points that are relevant in the sequence `seq`. It is
-important to use a variable time step (instead of constant sampling time) to increase the
-simulation speed.
+This function returns non-uniform time points that are relevant in the sequence `seq`.
+
+!!! note
+    It is important to use a variable time step (instead of constant sampling time) to
+    increase the simulation speed.
 
 # Arguments
-- `seq`: (::Sequence) the sequence object
-- `dt`: (::Real) the nominal delta time separation between two time samples for ADC
-    acquisition and Gradients (by nominal we mean that the time separation should be at
-    least `dt` when the samples are regarded by [`KomaMRI.is_ADC_on`](@ref) or
+- `seq`: (`::Sequence`) the sequence struct
+- `dt`: (`::Real`, `=1`, `[s]`) the nominal delta time separation between two time samples
+    for ADC acquisition and Gradients (by nominal we mean that the time separation should be
+    at most `Δt` when the samples are regarded by [`KomaMRI.is_ADC_on`](@ref) or
     [`KomaMRI.is_GR_on`](@ref)), otherwise the time points are not necessary and the
     separation will be bigger)
 
 # Keywords
-- `Δt_rf`: (::Real) the nominal delta time separation between two time samples for RF
-    excitation (by nominal we mean that the time separation should be at least `Δt_rf` when
-    the samples are regarded by [`KomaMRI.is_RF_on`](@ref), otherwise the time points are
-    not necessary and the separation will be bigger)
+- `Δt_rf`: (`::Real`, `=1e-4`, `[s]`) the nominal delta time separation between two time
+    samples for RF excitation (by nominal we mean that the time separation should be at most
+    `Δt_rf` when the samples are regarded by [`KomaMRI.is_RF_on`](@ref), otherwise the time
+    points are not necessary and the separation will be bigger)
 
 # Returns
-- `t`: (::Vector{Float64}) the time array with the non-uniform time values
-- `Δt`: (::Vector{Float64}) the delta time array with the separation between two adjacent
-    time points of the `t` array
+- `t`: (`::Vector{Float64}`, `[s]`) the time array with non-uniform time values
+- `Δt`: (`::Vector{Float64}`, `[s]`) the delta time array with the separation between two
+    adjacent time points of the `t` time array
 """
 function get_variable_times(seq; dt=1, dt_rf=1e-4)
 	t = Float64[]
@@ -232,20 +238,20 @@ end
 """
     key_idxs = get_breaks_in_RF_key_points(seq::Sequence, t)
 
-Return the indices of the time `t` array where are RF key points from a sequence `seq`.
-Thus, it is possible to split the simulation into RF and non-RF parts.
+Return the indices of the `t` time array where are RF key points from the `seq` sequence.
+Thus, it is possible to split the simulation into excitation and preccesion computations.
 
 !!! note
     By `RF key points` we mean all the start and end points where the RF excitation takes
     place with the [`KomaMRI.is_RF_on`](@ref) function.
 
 # Arguments
-- `seq::Sequence`: the sequence object
-- `t`: (::Int64) the non-uniform time array
+- `seq`: (`::Sequence`) the sequence struct
+- `t`: (`::Vector{Int64}`, `[s]`) the non-uniform time array
 
 # Returns
-- `key_idxs`: (::Vector{Int64}) array of indices of the time `t` array where are RF key
-    points.
+- `key_idxs`: (`::Vector{Int64}`) array of indices of the `t` time array where are RF key
+    points
 """
 function get_breaks_in_RF_key_points(seq::Sequence, t)
 	T0 = cumsum([0; durs(seq)[:]])
