3f4e31a681c9533d130ff3ad08736e2f11c08a57
Carlos Castillo Passi <cncastillo@uc.cl>
2022-12-29 18:42:28 +0000

    Cleaned lit, added download for .jl/.ipynb.

--- examples/lit-02-SmallTipApproximation.jl
+++ examples/lit-02-SmallTipApproximation.jl
@@ -1,22 +1,18 @@
 # # Small Tip Angle Approximation
 
-#md # [![](https://img.shields.io/badge/julia-script-9558B2?logo=julia)](@__REPO_ROOT_URL__/examples/lit-02-SmallTipApproximation.jl)
+#md # [![](https://img.shields.io/badge/julia-script-9558B2?logo=julia)](./FILE_NAME.jl)
+#md # [![](https://img.shields.io/badge/jupyter-notebook-blue?logo=jupyter)](./FILE_NAME.ipynb)
 
-filename = basename(@__FILE__) # hide
-isFileMD = occursin(".md", filename) # hide
-isFileJL = occursin(".jl", filename) # hide
-
-
-#md # > Based on the results in page 41 of the book "Handbook of MRI Pulse Sequences" by Bernstein et al.
+# > Based on the results in page 41 of the book "Handbook of MRI Pulse Sequences" by Bernstein et al.
 
 using KomaMRI # hide
 sys = Scanner() # hide
 sys.Smax = 50 # hide
 
-#md # In this example, we will showcase a common approximation in MRI, the small tip angle approximation. 
-#md # For this, we will simulate a slice profile for spins with positions ``z\in[-2,\,2]\,\mathrm{cm}``
-#md # and with a gradient ``G_{z}`` so their frequencies are mapped to ``f\in[-5,\,5]\,\mathrm{kHz}``.
-#md # To start, we define an RF pulse with a flip angle of 30 deg and pulse duration of ``T_{\mathrm{rf}}=3.2\,\mathrm{ms}``.
+# In this example, we will showcase a common approximation in MRI, the small tip angle approximation. 
+# For this, we will simulate a slice profile for spins with positions ``z\in[-2,\,2]\,\mathrm{cm}``
+# and with a gradient ``G_{z}`` so their frequencies are mapped to ``f\in[-5,\,5]\,\mathrm{kHz}``.
+# To start, we define an RF pulse with a flip angle of 30 deg and pulse duration of ``T_{\mathrm{rf}}=3.2\,\mathrm{ms}``.
 
 B1 = 4.92e-6
 Trf = 3.2e-3
@@ -26,21 +22,20 @@ z = range(-zmax, zmax, 400)
 Gz = fmax / (γ * zmax)
 f = γ * Gz * z # hide
 
-#md # The designed RF pulse is presented in the figure below, 
-#md # where the additional gradient refocuses the spins' phase after the excitation.  
+# The designed RF pulse is presented in the figure below, 
+# where the additional gradient refocuses the spins' phase after the excitation.  
 
 seq = PulseDesigner.RF_sinc(B1, Trf, sys; G=[0;0;Gz], TBP=8)
 p2 = plot_seq(seq; max_rf_samples=Inf, slider=false)
-if isFileMD savefig(p2, "../assets/42-seq.html") end # hide
-if isFileJL display(p2) end # hide
-nothing # hide
+#md savefig(p2, "../assets/42-seq.html") # hide
+#jl display(p2)
 
 #md # ```@raw html
 #md # <object type="text/html" data="../../assets/42-seq.html" style="width:100%; height:380px;"></object>
 #md # ```
 
-#md # Now we will perform the simulation using the function [`simulate_slice_profile`](@ref). 
-#md # Note that we modified `Δt_rf` in `simParams` to match the resolution of the waveform.
+# Now we will perform the simulation using the function [`simulate_slice_profile`](@ref). 
+# Note that we modified `Δt_rf` in `simParams` to match the resolution of the waveform.
 
 simParams = Dict{String, Any}("Δt_rf" => Trf / length(seq.RF.A[1]))
 M = simulate_slice_profile(seq; z, simParams)
@@ -59,9 +54,8 @@ FT_dat_pad = abs.(KomaMRI.fftc(dat_pad; dims=1)) # hide
 scale_factor = maximum(abs.(M.xy)) / maximum(FT_dat_pad) # hide
 s3 = scatter(x=u, y=FT_dat_pad*scale_factor, name="|FT(B₁(t))|", line=attr(dash="dash")) # hide
 pb = plot([s1,s2,s3], Layout(title="30 deg SINC pulse (TBP=8, Hamming)", xaxis_title="Frequency [Hz]", xaxis_range=[-fmax,fmax])) # hide
-if isFileMD savefig(pb, "../assets/4b-profile.html") end # hide
-if isFileJL display(pb) end # hide
-nothing # hide
+#md savefig(pb, "../assets/4b-profile.html") # hide
+#jl display(pb)
 
 #md # This produces the following slice profile:
 
@@ -69,10 +63,10 @@ nothing # hide
 #md # <object type="text/html" data="../../assets/4b-profile.html" style="width:100%; height:380px;"></object>
 #md # ```
 
-#md # As you can see, for a flip angle of 30 deg, the slice profile is very close to
-#md # the small tip angle approximation (the Fourier transform of ``B_{1}(t)``).
+# As you can see, for a flip angle of 30 deg, the slice profile is very close to
+# the small tip angle approximation (the Fourier transform of ``B_{1}(t)``).
 
-#md # But what will happen if we use a flip angle of 120 deg instead?
+# But what will happen if we use a flip angle of 120 deg instead?
 
 α_desired = 120 + 0im               # The multiplication of a complex number scales the RF pulse of a Sequence
 α = KomaMRI.get_flip_angles(seq)[1] # Previous FA approx 30 deg
@@ -91,15 +85,14 @@ FT_dat_pad = abs.(KomaMRI.fftc(dat_pad; dims=1)) # hide
 scale_factor = maximum(abs.(M.xy)) / maximum(FT_dat_pad) # hide
 s2 = scatter(x=u, y=FT_dat_pad*scale_factor, name="|FT(B₁(t))|", line=attr(dash="dash")) # hide
 pa = plot([s1,s2], Layout(title="120 deg SINC pulse (TBP=8, Hamming)", xaxis_title="Frequency [Hz]", xaxis_range=[-fmax,fmax])) # hide
-if isFileMD savefig(pa, "../assets/4a-profile.html") end # hide
-if isFileJL display(pa) end # hide
-nothing # hide
+#md savefig(pa, "../assets/4a-profile.html") # hide
+#jl display(pa)
 
 #md # ```@raw html
 #md # <object type="text/html" data="../../assets/4a-profile.html" style="width:100%; height:320px;"></object>
 #md # ```
 
-#md # For this case, the small tip angle approximation breaks 😢, thus, the reason for its name!
+# For this case, the small tip angle approximation breaks 😢, thus, the reason for its name!
 
-#md # This basic sinc pulse is not designed to be ``B_{1}``-insensitive.  Some adiabatic RF pulses have been proposed to achieve this.
-#md # Watch out for a future example showing these adiabatic RF pulses 👀.
+# This basic sinc pulse is not designed to be ``B_{1}``-insensitive.  Some adiabatic RF pulses have been proposed to achieve this.
+# Watch out for a future example showing these adiabatic RF pulses 👀.
