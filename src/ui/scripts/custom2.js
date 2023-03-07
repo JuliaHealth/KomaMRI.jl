483c1ecb387c5e1b9248dbef57711cfddd850026
Carlos Castillo Passi <cncastillo@uc.cl>
2023-03-07 15:07:44 +0000

    Added paper's DOI to the UI and fixed broken links

--- src/ui/scripts/custom2.js
+++ src/ui/scripts/custom2.js
@@ -1,3 +1,9 @@
 if (window.module) module = window.module;
 
 const { shell } = require('electron');
+
+//Open links in new Window
+mainWindow.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+});
