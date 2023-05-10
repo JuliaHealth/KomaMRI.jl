c6561f636cd9f04a87c17a4dcfb4112bebc8cd88
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2023-05-10 19:57:03 +0200

    Export read_phantom_file

--- KomaMRICore/src/KomaMRICore.jl
+++ KomaMRICore/src/KomaMRICore.jl
@@ -55,7 +55,7 @@ export read_seq
 #ISMRMRD
 export signal_to_raw_data
 #Phantom
-export brain_phantom2D, brain_phantom3D, read_phantom_jemris, read_phantom_MRiLab
+export brain_phantom2D, brain_phantom3D, read_phantom_jemris, read_phantom_MRiLab, read_phantom_file
 #Spinors
 export Spinor, Rx, Ry, Rz, Q, Un
 #Secondary
