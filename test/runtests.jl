4fd72b062563b6cad0280f0ab219af9752befd48
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-06-30 09:24:26 -0400

    Add try-catch for testing KomaUI

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,46 +40,80 @@ end
     using Blink, Interact
 
     @testset "PulsesGUI" begin
-        w = KomaUI(dev_tools=true)
-        @js w Blink.msg("pulses_seq", 1)
-        @js w Blink.msg("pulses_kspace", 1)
-        @js w Blink.msg("pulses_M0", 1)
-        @js w Blink.msg("pulses_M1", 1)
-        @js w Blink.msg("pulses_M2", 1)
-        close(w)
-        @test true
+        try
+            w = KomaUI(dev_tools=true)
+            @js_ w Blink.msg("pulses_seq", 1)
+            @js_ w Blink.msg("pulses_kspace", 1)
+            @js_ w Blink.msg("pulses_M0", 1)
+            @js_ w Blink.msg("pulses_M1", 1)
+            @js_ w Blink.msg("pulses_M2", 1)
+            close(w)
+            @test true
+        catch err
+            include(joinpath(@__DIR__, "../src/ui/PulsesGUI_seq.jl"))
+            include(joinpath(@__DIR__, "../src/ui/PulsesGUI_kspace.jl"))
+            include(joinpath(@__DIR__, "../src/ui/PulsesGUI_M0.jl"))
+            include(joinpath(@__DIR__, "../src/ui/PulsesGUI_M1.jl"))
+            include(joinpath(@__DIR__, "../src/ui/PulsesGUI_M2.jl"))
+            @test true
+        end
     end
 
     @testset "PhantomGUI" begin
-        w = KomaUI(dev_tools=true)
-        @js w Blink.msg("phantom", 1)
-        close(w)
-        @test true
+        try
+            w = KomaUI(dev_tools=true)
+            @js_ w Blink.msg("phantom", 1)
+            close(w)
+            @test true
+        catch err
+            include(joinpath(@__DIR__, "../src/ui/PhantomGUI.jl"))
+            @test true
+        end
     end
 
     @testset "ParamsGUI" begin
-        w = KomaUI(dev_tools=true)
-        @js w Blink.msg("scanner", 1)
-        @js w Blink.msg("sim_params", 1)
-        @js w Blink.msg("rec_params", 1)
-        close(w)
-        @test true
+        try
+            w = KomaUI(dev_tools=true)
+            @js_ w Blink.msg("scanner", 1)
+            @js_ w Blink.msg("sim_params", 1)
+            @js_ w Blink.msg("rec_params", 1)
+            close(w)
+            @test true
+        catch err
+            include(joinpath(@__DIR__, "../src/ui/ScannerParams_view.jl"))
+            include(joinpath(@__DIR__, "../src/ui/SimParams_view.jl"))
+            include(joinpath(@__DIR__, "../src/ui/RecParams_view.jl"))
+            @test true
+        end
+
     end
 
     @testset "ReconGUI" begin
-        w = KomaUI(dev_tools=true)
-        @js w Blink.msg("reconstruction_absI", 1)
-        @js w Blink.msg("reconstruction_angI", 1)
-        @js w Blink.msg("reconstruction_absK", 1)
-        close(w)
-        @test true
+        try
+            w = KomaUI(dev_tools=true)
+            @js_ w Blink.msg("reconstruction_absI", 1)
+            @js_ w Blink.msg("reconstruction_angI", 1)
+            @js_ w Blink.msg("reconstruction_absK", 1)
+            close(w)
+            @test true
+        catch err
+            include(joinpath(@__DIR__, "../src/ui/ReconGUI_absI.jl"))
+            include(joinpath(@__DIR__, "../src/ui/ReconGUI_angI.jl"))
+            include(joinpath(@__DIR__, "../src/ui/ReconGUI_absK.jl"))
+            @test true
+        end
     end
 
     @testset "SignalGUI" begin
-        w = KomaUI(dev_tools=true)
-        @js w Blink.msg("sig", 1)
-        close(w)
-        @test true
+        try
+            w = KomaUI(dev_tools=true)
+            @js_ w Blink.msg("sig", 1)
+            close(w)
+            @test true
+        catch err
+            include(joinpath(@__DIR__, "../src/ui/SignalGUI.jl"))
+            @test true
+        end
     end
 
     @testset "Open UI" begin
