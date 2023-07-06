79590e3581921fc9fb9077cbd61791df1e9f1d62
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-07-06 09:13:53 -0400

    Complete filter test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,36 +4,36 @@ using TestItems, TestItemRunner
 
 # include("../KomaMRICore/test/runtests.jl")
 # include("../KomaMRIPlots/test/runtests.jl")
-#
-#@testitem "MRIReco recon" tags=[:koma] begin
-#    #Sanity check 1
-#    A = rand(5,5,3)
-#    B = KomaMRI.fftc(KomaMRI.ifftc(A))
-#    @test A ≈ B
-#
-#    #Sanity check 2
-#    B = KomaMRI.ifftc(KomaMRI.fftc(A))
-#    @test A ≈ B
-#
-#    #MRIReco.jl
-#    path = @__DIR__
-#    fraw = ISMRMRDFile(path*"/test_files/Koma_signal.mrd")
-#    raw = RawAcquisitionData(fraw)
-#    acq = AcquisitionData(raw)
-#
-#    @testset "MRIReco_direct" begin
-#        Nx, Ny = raw.params["reconSize"][1:2]
-#        recParams = Dict{Symbol,Any}(:reco=>"direct", :reconSize=>(Nx,Ny), :densityWeighting=>true)
-#        img = reconstruction(acq, recParams)
-#        @test true                #If the previous line fails the test will fail
-#    end
-#
-#    #Test MRIReco regularized recon (with a λ)
-#    @testset "MRIReco_standard" begin
-#        #???
-#    end
-#
-#end
+
+@testitem "MRIReco recon" tags=[:koma] begin
+    #Sanity check 1
+    A = rand(5,5,3)
+    B = KomaMRI.fftc(KomaMRI.ifftc(A))
+    @test A ≈ B
+
+    #Sanity check 2
+    B = KomaMRI.ifftc(KomaMRI.fftc(A))
+    @test A ≈ B
+
+    #MRIReco.jl
+    path = @__DIR__
+    fraw = ISMRMRDFile(path*"/test_files/Koma_signal.mrd")
+    raw = RawAcquisitionData(fraw)
+    acq = AcquisitionData(raw)
+
+    @testset "MRIReco_direct" begin
+        Nx, Ny = raw.params["reconSize"][1:2]
+        recParams = Dict{Symbol,Any}(:reco=>"direct", :reconSize=>(Nx,Ny), :densityWeighting=>true)
+        img = reconstruction(acq, recParams)
+        @test true                #If the previous line fails the test will fail
+    end
+
+    #Test MRIReco regularized recon (with a λ)
+    @testset "MRIReco_standard" begin
+        #???
+    end
+
+end
 
 @testitem "KomaUI" tags=[:koma] begin
 
@@ -85,99 +85,95 @@ using TestItems, TestItemRunner
 
     end
 
-#    @testset "PhantomGUI" begin
-#        w = with_timeout(()->KomaUI(dev_tools=true; blink_show=false), 60)
-#        if !isnothing(w)
-#            @js w document.getElementById("button_phantom").click()
-#            close(w)
-#            @test true
-#        elseif VERSION < v"1.7"
-#            @test true
-#        else
-#            @test false
-#        end
-#    end
-#
-#    @testset "ParamsGUI" begin
-#        #w = KomaUI(dev_tools=true; blink_show=false)
-#        w = with_timeout(()->KomaUI(dev_tools=true; blink_show=false), 60)
-#        if !isnothing(w)
-#            @js w document.getElementById("button_scanner").click()
-#            @js w document.getElementById("button_sim_params").click()
-#            @js w document.getElementById("button_rec_params").click()
-#            close(w)
-#            @test true
-#        elseif VERSION < v"1.7"
-#            @test true
-#        else
-#            @test false
-#        end
-#    end
-#
-#    @testset "ReconGUI" begin
-#        #w = KomaUI(dev_tools=true; blink_show=false)
-#        w = with_timeout(()->KomaUI(dev_tools=true; blink_show=false), 60)
-#        if !isnothing(w)
-#            @js w document.getElementById("button_reconstruction_absI").click()
-#            @js w document.getElementById("button_reconstruction_angI").click()
-#            @js w document.getElementById("button_reconstruction_absK").click()
-#            close(w)
-#            @test true
-#        elseif VERSION < v"1.7"
-#            @test true
-#        else
-#            @test false
-#        end
-#    end
-#
-#    @testset "SignalGUI" begin
-#        #w = KomaUI(dev_tools=true; blink_show=false)
-#        w = with_timeout(()->KomaUI(dev_tools=true; blink_show=false), 60)
-#        if !isnothing(w)
-#            @js w document.getElementById("button_sig").click()
-#            close(w)
-#            @test true
-#        elseif VERSION < v"1.7"
-#            @test true
-#        else
-#            @test false
-#        end
-#    end
+    @testset "PhantomGUI" begin
+        w = with_timeout(()->KomaUI(dev_tools=true; blink_show=false), 60)
+        if !isnothing(w)
+            @js w document.getElementById("button_phantom").click()
+            close(w)
+            @test true
+        elseif VERSION < v"1.7"
+            @test true
+        else
+            @test false
+        end
+    end
+
+    @testset "ParamsGUI" begin
+        w = with_timeout(()->KomaUI(dev_tools=true; blink_show=false), 60)
+        if !isnothing(w)
+            @js w document.getElementById("button_scanner").click()
+            @js w document.getElementById("button_sim_params").click()
+            @js w document.getElementById("button_rec_params").click()
+            close(w)
+            @test true
+        elseif VERSION < v"1.7"
+            @test true
+        else
+            @test false
+        end
+    end
+
+    @testset "ReconGUI" begin
+        w = with_timeout(()->KomaUI(dev_tools=true; blink_show=false), 60)
+        if !isnothing(w)
+            @js w document.getElementById("button_reconstruction_absI").click()
+            @js w document.getElementById("button_reconstruction_angI").click()
+            @js w document.getElementById("button_reconstruction_absK").click()
+            close(w)
+            @test true
+        elseif VERSION < v"1.7"
+            @test true
+        else
+            @test false
+        end
+    end
+
+    @testset "SignalGUI" begin
+        w = with_timeout(()->KomaUI(dev_tools=true; blink_show=false), 60)
+        if !isnothing(w)
+            @js w document.getElementById("button_sig").click()
+            close(w)
+            @test true
+        elseif VERSION < v"1.7"
+            @test true
+        else
+            @test false
+        end
+    end
 
 end
 
-#@testitem "Auxiliar Functions" tags=[:koma] begin
-#    using MAT
-#    include(joinpath(@__DIR__, "../src/ui/ExportMATFunctions.jl"))
-#    @testset "ExportMATFunctions" begin
-#        phantom = brain_phantom2D()
-#        sys = Scanner()
-#        B1 = sys.B1; durRF = π/2/(2π*γ*B1) #90-degree hard excitation pulse
-#        EX = PulseDesigner.RF_hard(B1, durRF, sys; G=[0,0,0])
-#        N = 101
-#        FOV = 23e-2
-#        EPI = PulseDesigner.EPI(FOV, N, sys)
-#        TE = 30e-3
-#        d1 = TE-dur(EPI)/2-dur(EX)
-#        d1 = d1 > 0 ? d1 : 0
-#        if d1 > 0 DELAY = Delay(d1) end
-#        seq = d1 > 0 ? EX + DELAY + EPI : EX + EPI #Only add delay if d1 is positive (enough space)
-#        seq.DEF["TE"] = round(d1 > 0 ? TE : TE - d1, digits=4)*1e3
-#        path = @__DIR__
-#        fraw = ISMRMRDFile(path*"/test_files/Koma_signal.mrd")
-#        raw = RawAcquisitionData(fraw)
-#        acq = AcquisitionData(raw)
-#        Nx, Ny = raw.params["reconSize"][1:2]
-#        recParams = Dict{Symbol,Any}(:reco=>"direct", :reconSize=>(Nx,Ny), :densityWeighting=>true)
-#        aux = reconstruction(acq, recParams)
-#        image  = reshape(aux.data, Nx, Ny, :)
-#        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="all")
-#        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="sequence")
-#        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="phantom")
-#        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="scanner")
-#        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="raw")
-#        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="image")
-#        @test true
-#    end
-#end
-#
+@testitem "Auxiliar Functions" tags=[:koma] begin
+    using MAT
+    include(joinpath(@__DIR__, "../src/ui/ExportMATFunctions.jl"))
+    @testset "ExportMATFunctions" begin
+        phantom = brain_phantom2D()
+        sys = Scanner()
+        B1 = sys.B1; durRF = π/2/(2π*γ*B1) #90-degree hard excitation pulse
+        EX = PulseDesigner.RF_hard(B1, durRF, sys; G=[0,0,0])
+        N = 101
+        FOV = 23e-2
+        EPI = PulseDesigner.EPI(FOV, N, sys)
+        TE = 30e-3
+        d1 = TE-dur(EPI)/2-dur(EX)
+        d1 = d1 > 0 ? d1 : 0
+        if d1 > 0 DELAY = Delay(d1) end
+        seq = d1 > 0 ? EX + DELAY + EPI : EX + EPI #Only add delay if d1 is positive (enough space)
+        seq.DEF["TE"] = round(d1 > 0 ? TE : TE - d1, digits=4)*1e3
+        path = @__DIR__
+        fraw = ISMRMRDFile(path*"/test_files/Koma_signal.mrd")
+        raw = RawAcquisitionData(fraw)
+        acq = AcquisitionData(raw)
+        Nx, Ny = raw.params["reconSize"][1:2]
+        recParams = Dict{Symbol,Any}(:reco=>"direct", :reconSize=>(Nx,Ny), :densityWeighting=>true)
+        aux = reconstruction(acq, recParams)
+        image  = reshape(aux.data, Nx, Ny, :)
+        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="all")
+        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="sequence")
+        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="phantom")
+        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="scanner")
+        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="raw")
+        export_2_mat(seq, phantom, sys, raw, recParams, image, pwd(); type="image")
+        @test true
+    end
+end
