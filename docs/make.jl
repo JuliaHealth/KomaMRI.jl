eb37cc9841db3432590196515620eee27deed0a5
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-08-10 18:47:26 -0400

    Use 2 folders for lierate examples in docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,19 +8,21 @@ repo_root_url = "https://github.com/$base/blob/master"
 exa = joinpath(@__DIR__, "../examples")
 src = joinpath(@__DIR__, "src")
 gen = joinpath(@__DIR__, "src/generated")
-folname_bas = "basic"
-bas = joinpath(exa, folname_bas)
+foldernames = ["basic"; "sequence-design"]
+folderpaths = [joinpath(exa, foldername) for foldername in foldernames]
 
-function update_file_name(file)
-    return content -> content = replace(content, "FILE_NAME" => file)
+function preprocess_constants(file, folder)
+    return content -> content = replace(content, "FILE_NAME" => file, "FOLDER_NAME" => folder)
 end
 
-for (root, _, files) ∈ walkdir(bas), file ∈ files
-    file_minus_ext = split(file, ".")[1] #lit-02-example.jl => lit-02-example
-    ipath, opath = joinpath(root, file), joinpath(gen, folname_bas)
-    Literate.markdown(ipath, opath; repo_root_url, preprocess=update_file_name(file_minus_ext) )
-    Literate.script(ipath, opath; repo_root_url)
-    Literate.notebook(ipath, opath; execute=false)
+for i ∈ eachindex(foldernames)
+    for (root, _, files) ∈ walkdir(folderpaths[i]), file ∈ files
+        file_minus_ext = split(file, ".")[1]
+        ipath, opath = joinpath(root, file), joinpath(gen, foldernames[i])
+        Literate.markdown(ipath, opath; repo_root_url, preprocess=preprocess_constants(file_minus_ext, foldernames[i]) )
+        Literate.script(ipath, opath; repo_root_url)
+        Literate.notebook(ipath, opath; execute=false)
+    end
 end
 
 # Documentation structure
@@ -33,11 +35,12 @@ makedocs(
     authors = "Boris Orostica Navarrete and Carlos Castillo Passi",
     pages = [
         "Home" => "index.md",
-        "Getting Started" => "getting-started.md",
-        "Graphical User Interface" => "ui-details.md",
-        "Examples" => pages(folname_bas),
-        "Simulation Method" => "mri-theory.md",
-        "API Documentation" => "api.md"
+        #"Getting Started" => "getting-started.md",
+        #"Graphical User Interface" => "ui-details.md",
+        "Examples" => pages(foldernames[1]),
+        "Sequence Design" => pages(foldernames[2]),
+        #"Simulation Method" => "mri-theory.md",
+        #"API Documentation" => "api.md"
     ],
     format = Documenter.HTML(
         prettyurls = true, #get(ENV, "CI", nothing) == "true",
---
+++ examples/sequence-design/02-GR.jl
@@ -0,0 +1,40 @@
+# # GR Design
+
+#md # [![](https://img.shields.io/badge/julia-script-9558B2?logo=julia)](./FILE_NAME.jl)
+#md # [![](https://img.shields.io/badge/jupyter-notebook-blue?logo=jupyter)](./FILE_NAME.ipynb)
+
+# First of all, let's use the KomaMRI package and define the default scanner.
+
+using KomaMRI
+sys = Scanner() # default hardware definition
+
+# The free induction decay is the simplest observable NMR signal.
+# This signal is the one that follows a single tipping RF pulse.
+# To recreate this experiment, we will need to define a `Sequence` with 2 blocks.
+
+# The first block containing an RF pulse with a flip-angle of 90 deg,
+
+ampRF = 2e-6                        # 2 uT RF amplitude
+durRF = π / 2 / (2π * γ * ampRF)    # required duration for a 90 deg RF pulse
+exc = RF(ampRF,durRF)
+
+# and the second block containing the ADC.
+
+nADC = 8192         # number of acquisition samples
+durADC = 250e-3     # duration of the acquisition
+delay =  1e-3       # small delay
+acq = ADC(nADC, durADC, delay)
+
+# Finally, we concatenate the sequence blocks to create
+# the final sequence.
+
+seq = Sequence()  # empty sequence
+seq += exc        # adding RF-only block
+seq += acq        # adding ADC-only block
+p1 = plot_seq(seq; slider=false, height=300)
+#md savefig(p1, "../../assets/FOLDER_NAME/2-seq.html") # hide
+#jl display(p1)
+
+#md # ```@raw html
+#md # <object type="text/html" data="../../../assets/FOLDER_NAME/2-seq.html" style="width:100%; height:320px;"></object>
+#md # ```
