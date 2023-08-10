30ef2fdfc4e6efa733d5fdff933aaef7aa686d7d
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-08-10 17:27:59 -0400

    Change literate examples folder and fix some unresolved links in the docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,25 +8,24 @@ repo_root_url = "https://github.com/$base/blob/master"
 exa = joinpath(@__DIR__, "../examples")
 src = joinpath(@__DIR__, "src")
 gen = joinpath(@__DIR__, "src/generated")
+folname_bas = "basic"
+bas = joinpath(exa, folname_bas)
 
 function update_file_name(file)
     return content -> content = replace(content, "FILE_NAME" => file)
 end
 
-for (root, _, files) ∈ walkdir(exa), file ∈ files
-    #splitext(file)[2] == ".jl" || continue
-    cmp(last(splitpath(root)), "examples") == 0 && cmp(first(split(file, "-")), "lit") == 0 || continue
+for (root, _, files) ∈ walkdir(bas), file ∈ files
     file_minus_ext = split(file, ".")[1] #lit-02-example.jl => lit-02-example
-    ipath = joinpath(root, file)
-    Literate.markdown(ipath, gen; repo_root_url, preprocess=update_file_name(file_minus_ext) )
-    Literate.script(ipath, gen; repo_root_url)
-    Literate.notebook(ipath, gen; execute=false)
+    ipath, opath = joinpath(root, file), joinpath(gen, folname_bas)
+    Literate.markdown(ipath, opath; repo_root_url, preprocess=update_file_name(file_minus_ext) )
+    Literate.script(ipath, opath; repo_root_url)
+    Literate.notebook(ipath, opath; execute=false)
 end
 
 # Documentation structure
 ismd(f) = splitext(f)[2] == ".md"
-pages() = [joinpath("generated", f) for f in readdir(gen) if ismd(f)]
-#pages(folder) = [joinpath("generated", folder, f) for f in readdir(joinpath(gen, folder)) if ismd(f)]
+pages(folder) = [joinpath("generated", folder, f) for f in readdir(joinpath(gen, folder)) if ismd(f)]
 
 makedocs(
     modules = [KomaMRI, KomaMRICore, KomaMRIPlots],
@@ -35,12 +34,8 @@ makedocs(
     pages = [
         "Home" => "index.md",
         "Getting Started" => "getting-started.md",
-        # "Sequence" => "sequence.md",
-        # "Phantom" => "phantom.md",
-        # "Scanner" => "scanner.md",
-        # "Raw Signal" => "raw-signal.md",
         "Graphical User Interface" => "ui-details.md",
-        "Examples" => pages(),
+        "Examples" => pages(folname_bas),
         "Simulation Method" => "mri-theory.md",
         "API Documentation" => "api.md"
     ],
