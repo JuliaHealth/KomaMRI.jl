7464a9d81b2e1c94f0ef5b2646b16f89c07896bd
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-10-05 12:17:05 -0300

    Add temporal changes to literate docs

--- examples/literate/sequence-design/01-GR.jl
+++ examples/literate/sequence-design/01-GR.jl
@@ -1,16 +1,16 @@
-# # GR Design
+# # Gradient Design
 
 #md # [![](https://img.shields.io/badge/julia-script-9558B2?logo=julia)](./FILE_NAME.jl)
 #md # [![](https://img.shields.io/badge/jupyter-notebook-blue?logo=jupyter)](./FILE_NAME.ipynb)
 
-# Let's import the KomaMRI package
+# Before doing anything, let's import the KomaMRI package
 
 using KomaMRI
 
-# ## GR Trapezoidal definition
+# ## Gradient Trapezoidal Waveform
 
 # The most simple definition of a gradient is a trapezoidal waveform. Let's create a sequence
-# (a sequence with just one block), with 3 x,y,z gradients:
+# (a sequence with just one block), with 3 gradients:
 
 ## Define parameters and gradients
 ## A: amplitude of the gradient
@@ -36,33 +36,22 @@ p1 = plot_seq(seq; slider=false, height=300)
 #md # <object type="text/html" data="../../../assets/FOLDER_NAME/1-gr-trap.html" style="width:100%; height:320px;"></object>
 #md # ```
 
-seq.GR[1,1]
-seq.GR[1,1].A
-seq.GR[1,1].T
-seq.GR[1,1].rise
-seq.GR[1,1].fall
 
-seq.GR[2,1]
-seq.GR[2,1].A
-seq.GR[2,1].T
-seq.GR[2,1].rise
-seq.GR[2,1].fall
+# ## Gradient Uniformly-Sampled Waveform
 
-seq.GR[3,1]
-seq.GR[3,1].A
-seq.GR[3,1].T
-seq.GR[3,1].rise
-seq.GR[3,1].fall
+# You can also create gradients waveforms with more flexible shapes. Let's create a gradient
+# in one axis, with an arbritary amplitude shape at evenly spaced times:
 
-# ## GR Arbitrary waveform
+## Define parameters
+## T: duration of the arbitrary waveform
+## ζ: rise and fall duration
+## A: vector with the amplitudes of the gradient
+T, ζ = 1e-3, 0.05e-3
+A = 10e-3 * (sin.(0.:2π/10:2π) .+ 1)
 
-A = 10e-3 * [1; .65; .2; .1; .2; .65; 1]
-T, ζ = 1e-3, 0.2e-3
-gx = Grad(   A, T, ζ)
-gy = Grad( 2*A, T, ζ)
-gz = Grad(.5*A, T, ζ)
-gr_m = reshape([gx; gy; gz], :, 1)
-seq = Sequence(gr_m)
+## Define the gradient and the sequence (with just one block)
+gx = Grad(A, T, ζ)
+seq = Sequence([gx])
 
 p2 = plot_seq(seq; slider=false, height=300)
 #md savefig(p2, "../../assets/FOLDER_NAME/2-gr-wave.html") # hide
@@ -72,10 +61,35 @@ p2 = plot_seq(seq; slider=false, height=300)
 #md # <object type="text/html" data="../../../assets/FOLDER_NAME/2-gr-wave.html" style="width:100%; height:320px;"></object>
 #md # ```
 
-# ## GR x y z concatenation
+# Note that the waveform always defines rise and fall times, so the gradients always starts
+# and ends at zero amplitude.
 
-# ## GR time concatenation
 
-# ## GR rotation
+# ## Gradient Time-Shaped Waveform
 
-# ## GR example: moment compensated diffusion
+# You can also create a waveform sampled at arbitraty time positions (as long as they are in
+# increasing order). Let's create such gradient:
+
+## Define parameters
+## A: vector with the amplitudes of the gradient
+## T: vector with the distances between two consecutive time samples
+## ζ: rise and fall duration
+
+A = 10e-3 * (sin.(0.:π/10:π) .+ 1)
+T = 10e-3 * [((-1:0.2:-0.2).^2); ((0.2:0.2:1).^2)]
+ζ = 0.05e-3
+
+## Define the gradient and the sequence (with just one block)
+gx = Grad(A, T, ζ)
+seq = Sequence([gx])
+
+p3 = plot_seq(seq; slider=false, height=300)
+#md savefig(p3, "../../assets/FOLDER_NAME/3-gr-wave.html") # hide
+#jl display(p3)
+
+#md # ```@raw html
+#md # <object type="text/html" data="../../../assets/FOLDER_NAME/3-gr-wave.html" style="width:100%; height:320px;"></object>
+#md # ```
+
+# Note that in this case the length of the amplitud vector must have one sample more than the
+# samples of the vector of time distances.
