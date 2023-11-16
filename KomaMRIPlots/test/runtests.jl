a019ec607efb328a3df175a1aa6c48e756086344
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-11-16 10:28:57 -0300

    Define dummy KomaMRIPlots just for testing subdir in windows CI

--- KomaMRIPlots/test/runtests.jl
+++ KomaMRIPlots/test/runtests.jl
@@ -3,5 +3,13 @@ using TestItems, TestItemRunner
 @run_package_tests filter=ti->!(:skipci in ti.tags)&&(:plots in ti.tags) #verbose=true
 
 #GUI tests
-include("GUI_PlotlyJS_backend_test.jl")
-include("GUI_PlutoPlotly_backend_test.jl")
+#include("GUI_PlotlyJS_backend_test.jl")
+#include("GUI_PlutoPlotly_backend_test.jl")
+
+@testitem "Test_Dummy" tags=[:plots] begin
+    @testset "get_seq" begin
+        seq = get_seq()
+        @test true
+    end
+
+end
