387b0402d447f4f734881d91a6381abbec86e3a5
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-11-16 12:17:17 -0300

    Remove the use of PackageExtensionCompart for testing CI

--- KomaMRIPlots/src/KomaMRIPlots.jl
+++ KomaMRIPlots/src/KomaMRIPlots.jl
@@ -13,7 +13,7 @@ export plot_seq, plot_M0, plot_M1, plot_M2, plot_eddy_currents, plot_seqd,
         plot_slew_rate, plot_kspace, plot_phantom_map, plot_signal, plot_image, plot_dict
 
 #PlutoPlotly extension
-using PackageExtensionCompat
+#using PackageExtensionCompat
 #function __init__()
 #        @require_extensions
 #end
