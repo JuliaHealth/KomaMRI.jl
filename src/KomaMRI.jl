640c3b99814f310759a1c18d6a618b0cda510370
Carlos Castillo Passi <cncastillo@uc.cl>
2023-11-17 07:12:14 -0300

    Removed comment from KomaMRI.jl

--- src/KomaMRI.jl
+++ src/KomaMRI.jl
@@ -28,8 +28,6 @@ export sys_ui, seq_ui, obj_ui, raw_ui, img_ui
 
 #Package version, KomaMRI.__VERSION__
 using Pkg
-__VERSION__ = Pkg.project().version
-
-# beorostica was here
+__VERSION__ = VersionNumber(Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))["version"])
 
 end
