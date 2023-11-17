66bb0d8f0c1b2ad737c26855bc65b2198cf293ac
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-11-17 05:27:18 -0300

    Uncomment some lines form KomaMRIPlos module for testing CI

--- KomaMRIPlots/src/KomaMRIPlots.jl
+++ KomaMRIPlots/src/KomaMRIPlots.jl
@@ -4,10 +4,10 @@ using KomaMRICore
 using MAT, Interpolations, PlotlyJS
 
 #include("ui/PlotBackends.jl")
-include("ui/DisplayFunctions.jl")
+#include("ui/DisplayFunctions.jl")
 
-#using Reexport
-#@reexport using PlotlyJS: savefig
+using Reexport
+@reexport using PlotlyJS: savefig
 
 #export plot_seq, plot_M0, plot_M1, plot_M2, plot_eddy_currents, plot_seqd,
 #        plot_slew_rate, plot_kspace, plot_phantom_map, plot_signal, plot_image, plot_dict
@@ -19,7 +19,7 @@ include("ui/DisplayFunctions.jl")
 #end
 
 #Package version, KomaMRIPlots.__VERSION__
-#using Pkg
-#__VERSION__ = VersionNumber(Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))["version"])
+using Pkg
+__VERSION__ = VersionNumber(Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))["version"])
 
 end
