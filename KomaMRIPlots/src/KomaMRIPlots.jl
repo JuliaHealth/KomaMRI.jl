92ec515fb9b910a60ca1ad7ca089a1a893d40eae
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-11-17 12:41:13 -0300

    Comment-out code lines in KomaMRIPlos for testing CI

--- KomaMRIPlots/src/KomaMRIPlots.jl
+++ KomaMRIPlots/src/KomaMRIPlots.jl
@@ -3,14 +3,14 @@ module KomaMRIPlots
 using KomaMRICore
 using MAT, Interpolations, PlotlyJS
 
-include("ui/PlotBackends.jl")
+#include("ui/PlotBackends.jl")
 #include("ui/DisplayFunctions.jl")
 
 #using Reexport
 #@reexport using PlotlyJS: savefig
 
 #export plot_seq, plot_M0, plot_M1, plot_M2, plot_eddy_currents, plot_seqd,
-#        plot_slew_rate, plot_kspace, plot_phantom_map, plot_signal, plot_image, plot_dict
+        plot_slew_rate, plot_kspace, plot_phantom_map, plot_signal, plot_image, plot_dict
 
 #PlutoPlotly extension
 #using PackageExtensionCompat
