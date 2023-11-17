7c8b46dc54965dacd9c9fbb0543167075f23fe20
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-11-17 09:30:53 -0300

    Remove display functions for testing CI

--- KomaMRIPlots/src/KomaMRIPlots.jl
+++ KomaMRIPlots/src/KomaMRIPlots.jl
@@ -4,13 +4,13 @@ using KomaMRICore
 using MAT, Interpolations, PlotlyJS
 
 include("ui/PlotBackends.jl")
-include("ui/DisplayFunctions.jl")
+#include("ui/DisplayFunctions.jl")
 
 using Reexport
 @reexport using PlotlyJS: savefig
 
-export plot_seq, plot_M0, plot_M1, plot_M2, plot_eddy_currents, plot_seqd,
-        plot_slew_rate, plot_kspace, plot_phantom_map, plot_signal, plot_image, plot_dict
+#export plot_seq, plot_M0, plot_M1, plot_M2, plot_eddy_currents, plot_seqd,
+#        plot_slew_rate, plot_kspace, plot_phantom_map, plot_signal, plot_image, plot_dict
 
 #PlutoPlotly extension
 using PackageExtensionCompat
