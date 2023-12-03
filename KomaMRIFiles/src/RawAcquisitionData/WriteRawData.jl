029524ebd6ae62b51bc2bb6f82bed7117367302b
Carlos Castillo Passi <cncastillo@uc.cl>
2023-12-03 18:43:07 -0300

    MRIFiles: defining Limit is not necessary now

--- KomaMRIFiles/src/RawAcquisitionData/WriteRawData.jl
+++
@@ -1,6 +0,0 @@
-"""
-For using the Limit Struct of KomaMRICore when writing with MRIFiles
-"""
-insertNode(xs, paramName::String, param::KomaMRICore.Limit) = begin
-    insertNode(xs, paramName, MRIFiles.Limit(param.minimum, param.maximum, param.center))
-end
