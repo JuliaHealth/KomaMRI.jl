ef2f5960eed39cd0ef1c76b3b573299beda6528a
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-12-12 10:17:20 -0300

    Define parameters for EPI example in KomaMRICore tests

--- KomaMRICore/test/test_files/Utils.jl
+++ KomaMRICore/test/test_files/Utils.jl
@@ -20,13 +20,39 @@ function seq_epi_100x100_TE100_FOV230()
 
     # First Block (excitation)
     seq = Sequence()
-    rf = RF((5.871650124959988e-5 - 0.0im)*ones(99), 9.9e-5, 5.0e-7, 0.0)
-    seq += rf; seq = seq[2:end]
-    seq[1].DUR[1] = 9.949999999999999e-5
+    Trf = 1e-4                     # From File: 9.9e-5
+    Arf = 1/(4*γ*Trf)              # From File: 5.871650124959988e-5
+    rf = RF(Arf, Trf)              # From File: RF(Arf*ones(99), Trf, 5.0e-7, 0.0)
+    seq += rf; seq = seq[2:end]    # From File: seq[1].DUR[1] = 9.949999999999999e-5
+
+    # Define params
+    begin
+        # Image params
+        FOVx = 0.230                    # Set
+        Nx = 100                        # Set
+        FOVy = 0.230                    # Set
+        Ny = 100                        # Set
+
+        # For gradients which moves in the k-space (and ADC)
+        Tgx = 0.001                     # Set
+        ζgx = 0.00028                   # Set
+        Δtadc_min = 0.00001             # Set
+        Agx = Nx/(γ*Tgx*FOVx)           # From File: 0.010211565230481714
+        Tadc = Tgx - Δtadc_min          # From File: 0.00099
+        ΔDadc = ζgx + Δtadc_min/2       # From File: 0.000285
+        ζgy = 6e-5                      # From File: 5.9999999999999995e-5
+        Agy = 1/(γ*ζgy*FOVy)            # From File: 0.0017019276167022875
+
+        # For first corner in the k-space
+        ζgyo = 0.00037                  # Set
+        ζgxo = 0.00042                  # From File: 0.00041999999999999996
+        Agyo = 0.5*Ny/(γ*ζgyo*FOVy)     # From File: 0.013799413552738015
+        Agxo = 0.5*Agx*(Tgx + ζgx)/ζgxo # From File: 0.015560481134096917
+    end
 
     # Second Block (move to a corner of the k-space)
-    gx = Grad(-0.015560481134096917, 0.0, 0.00041999999999999996, 0.00041999999999999996, 0.0)
-    gy = Grad(0.013799413552738015, 0.0, 0.00037, 0.00037, 0.0)
+    gx = Grad(-Agxo, 0.0, ζgxo)
+    gy = Grad(Agyo, 0.0, ζgyo)
     gz = Grad(0.0, 0.0)
     seq += Sequence(reshape([gx; gy; gz], :, 1))
 
@@ -36,21 +62,21 @@ function seq_epi_100x100_TE100_FOV230()
 
     # Define blocks that will be repeated
     begin
-        gx = Grad(0.010211565230481714, 0.001, 0.00028, 0.00028, 0.0)
+        gx = Grad(Agx, Tgx, ζgx)
         gy = Grad(0.0, 0.0)
         gz = Grad(0.0, 0.0)
         rf = RF(0.0, 0.0)
-        adc = ADC(100, 0.00099, 0.000285, 0.0, 0.0)
+        adc = ADC(Nx, Tadc, ΔDadc)
         sxA = Sequence(reshape([gx; gy; gz], :, 1), reshape([rf], :, 1), [adc])
         sxB = Sequence(reshape([-gx; gy; gz], :, 1), reshape([rf], :, 1), [adc])
         gx = Grad(0.0, 0.0)
-        gy = Grad(-0.0017019276167022875, 0.0, 5.9999999999999995e-5, 5.9999999999999995e-5, 0.0)
+        gy = Grad(-Agy, 0.0, ζgy)
         gz = Grad(0.0, 0.0)
         syC = Sequence(reshape([gx; gy; gz], :, 1))
     end
 
     # Repetition of blocks (move in the k-space)
-    for _ in 1:49
+    for _ in 1:(round(Int, Ny/2)-1)
         seq += sxA
         seq += syC
         seq += sxB
