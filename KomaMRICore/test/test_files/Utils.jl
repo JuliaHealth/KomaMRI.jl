1a1a63a0e27eee8538d846d61c16fb6758fa8cd5
beorostica-asusFX505GT-win <beorostica@uc.cl>
2023-12-14 16:05:27 -0300

    Update comments in the JEMRIS test sequence

--- KomaMRICore/test/test_files/Utils.jl
+++ KomaMRICore/test/test_files/Utils.jl
@@ -20,50 +20,47 @@ function seq_epi_100x100_TE100_FOV230()
 
     # First Block (excitation)
     seq = Sequence()
-    Trf = 1e-4                     # From File: 9.9e-5
-    Arf = (π/2)/(2π*γ*Trf)         # From File: 5.871650124959988e-5. 90° rect excitation pulse
-    rf = RF(Arf, Trf)              # From File: RF(Arf*ones(99), Trf, 5.0e-7, 0.0)
-    seq += rf;                     # From File: seq[1].DUR[1] = 9.949999999999999e-5
+    Trf = 100e-6                   # RF duration 100us
+    Arf = (π/2)/(2π*γ*Trf)         # RF Amplitude for 90° rect excitation pulse
+    rf = RF(Arf, Trf)              # RF struct definition
+    seq += rf;                     # Append the RF to the empty sequence
 
-    # Define params
-    begin
-        # Gyromagnetic constant
-        γ_jemris = 2π*γ/1000000000      # γ in [Hz/T]. γ_jemris in [rad/us/mT]
+    # Gyromagnetic constant
+    γ_jemris = 2π*γ*1e-9            # γ in [Hz/T]. γ_jemris in [rad/us/mT]
 
-        # User params (defined in the original JEMRIS XML file)
-        FOVx = 0.230                    # Set
-        Nx = 100                        # Set
-        FOVy = 0.230                    # Set
-        Ny = 100                        # Set
-        TE = 0.1                        # Set
-        Δtadc = 0.00001                 # Set
-        Smax = 10/γ_jemris              # Set
-        Δtgr_digits = 5                 # Time resolution for gradients defined in JEMRIS source code (10us)
+    # User params (defined in the original JEMRIS XML file)
+    FOVx = 230e-3                   # 230mm Field-of-View in x
+    Nx = 100                        # Number of points in x
+    FOVy = 230e-3                   # 230mm Field-of-View in y
+    Ny = 100                        # Number of points in y
+    TE = 100e-3                     # 100ms Time to Echo
+    Δtadc = 10e-6                   # 10us ADC sampling time
+    Smax = 10/γ_jemris              # Gradient slew rate in (T/m/s)
+    Δtgr_digits = 5                 # Time resolution for gradients defined in JEMRIS source code (10us)
 
-        # For gradients which moves in the k-space (and ADC)
-        Area_adc = Nx/FOVx/γ                                # The width of the k-space traversed by the ADC in the x-direction
-        Tgx = Nx*Δtadc                                      # The duration of the flat top of the x-gradient
-        Agx = Area_adc/Tgx                                  # The amplitude of the x-gradient
-        ζgx = ceil(Agx/Smax; digits=Δtgr_digits)            # The rise and fall time of the the x-gradient
-        Tadc = Tgx - Δtadc                                  # The duration of the ADC
-        ΔDadc = ζgx + Δtadc/2                               # The delay of the ADC
-        Area_gy = 1/FOVy/γ                                  # The k-space resolution in the y-direction
-        ζgy = ceil(sqrt(Area_gy/Smax); digits=Δtgr_digits)  # The rise and fall time of the the triangular y-gradient (ζ = sqrt(Area/Slewrate) = sqrt((G*ζ)/(G/ζ)))
-        Agy = Area_gy/ζgy                                   # The amplitude of the triangular y-gradient
+    # For gradients which moves in the k-space (and ADC)
+    Area_adc = Nx/FOVx/γ                                # The width of the k-space traversed by the ADC in the x-direction
+    Tgx = Nx*Δtadc                                      # The duration of the flat top of the x-gradient
+    Agx = Area_adc/Tgx                                  # The amplitude of the x-gradient
+    ζgx = ceil(Agx/Smax; digits=Δtgr_digits)            # The rise and fall time of the the x-gradient
+    Tadc = Tgx - Δtadc                                  # The duration of the ADC
+    ΔDadc = ζgx + Δtadc/2                               # The delay of the ADC
+    Area_gy = 1/FOVy/γ                                  # The k-space resolution in the y-direction
+    ζgy = ceil(sqrt(Area_gy/Smax); digits=Δtgr_digits)  # The rise and fall time of the the triangular y-gradient (ζ = sqrt(Area/Slewrate) = sqrt((G*ζ)/(G/ζ)))
+    Agy = Area_gy/ζgy                                   # The amplitude of the triangular y-gradient
 
-        # For first corner in the k-space (half of the covered rectangle in the k-space)
-        Area_kx = Area_adc + Agx*ζgx                            # The width of the k-space traversed by the x-gradient in the x-direction
-        Area_gxo = 0.5*Area_kx                                  # The half-width of the k-space in the x-direction
-        ζgxo = ceil(sqrt(Area_gxo/Smax); digits=Δtgr_digits)    # The rise and fall time of the the triangular dephasing x-gradient (ζ = sqrt(Area/Slewrate) = sqrt((G*ζ)/(G/ζ)))
-        Agxo = Area_gxo/ζgxo                                    # The amplitude of the triangular dephasing x-gradient
-        Area_ky = Ny*Area_gy                                    # The width of the k-space traversed by the y-gradient in the y-direction
-        Area_gyo = 0.5*Area_ky                                  # The half-width of the k-space in the y-direction
-        ζgyo = ceil(sqrt(Area_gyo/Smax); digits=Δtgr_digits)    # The rise and fall time of the the triangular dephasing y-gradient (ζ = sqrt(Area/Slewrate) = sqrt((G*ζ)/(G/ζ)))
-        Agyo = Area_gyo/ζgyo                                    # The amplitude of the triangular dephasing y-gradient
+    # For first corner in the k-space (half of the covered rectangle in the k-space)
+    Area_kx = Area_adc + Agx*ζgx                            # The width of the k-space traversed by the x-gradient in the x-direction
+    Area_gxo = 0.5*Area_kx                                  # The half-width of the k-space in the x-direction
+    ζgxo = ceil(sqrt(Area_gxo/Smax); digits=Δtgr_digits)    # The rise and fall time of the the triangular dephasing x-gradient (ζ = sqrt(Area/Slewrate) = sqrt((G*ζ)/(G/ζ)))
+    Agxo = Area_gxo/ζgxo                                    # The amplitude of the triangular dephasing x-gradient
+    Area_ky = Ny*Area_gy                                    # The width of the k-space traversed by the y-gradient in the y-direction
+    Area_gyo = 0.5*Area_ky                                  # The half-width of the k-space in the y-direction
+    ζgyo = ceil(sqrt(Area_gyo/Smax); digits=Δtgr_digits)    # The rise and fall time of the the triangular dephasing y-gradient (ζ = sqrt(Area/Slewrate) = sqrt((G*ζ)/(G/ζ)))
+    Agyo = Area_gyo/ζgyo                                    # The amplitude of the triangular dephasing y-gradient
 
-        # For dead time dalay to match TE (taken from JEMRIS center-to-center TE definition)
-        ΔD = TE - (Trf/2 + 2*ζgxo + Ny*ζgx + Ny*Tgx/2 + (Ny-1)*ζgy)
-    end
+    # For dead time dalay to match TE (taken from JEMRIS center-to-center TE definition)
+    ΔD = TE - (Trf/2 + 2*ζgxo + Ny*ζgx + Ny*Tgx/2 + (Ny-1)*ζgy)
 
     # Second Block (move to a corner of the k-space)
     gx = Grad(-Agxo, 0.0, ζgxo)
@@ -90,10 +87,10 @@ function seq_epi_100x100_TE100_FOV230()
 
     # Repetition of blocks (move in the k-space)
     for _ in 1:(Ny÷2-1)
-        seq += sxA  # moves to the right in kx and samples the signal
-        seq += syC  # moves a blip down in ky
-        seq += sxB  # moves to the left in kx and samples the signal
-        seq += syC  # moves a blip down in ky again
+        seq += sxA  # readout: moves to the right in kx and samples the signal
+        seq += syC  # blip_neg: moves a blip down in ky
+        seq += sxB  # readout: moves to the left in kx and samples the signal
+        seq += syC  # blip_neg: moves a blip down in ky again
     end
     seq += sxA  # moves to the right in kx and samples the signal
     seq += syC  # moves a blip down in ky
