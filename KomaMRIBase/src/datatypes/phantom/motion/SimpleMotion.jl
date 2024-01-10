4189774670e8e0b4efe7ce401b823537b5d550e5
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-01-10 09:37:22 +0100

    Remove unused functions

--- KomaMRIBase/src/datatypes/phantom/motion/SimpleMotion.jl
+++ KomaMRIBase/src/datatypes/phantom/motion/SimpleMotion.jl
@@ -1,51 +1,23 @@
-@with_kw mutable struct SimpleMotion{T} <: MotionModel{T}
+@with_kw mutable struct SimpleMotion <: MotionModel
     ux::Function = (x,y,z,t)->0
 	uy::Function = (x,y,z,t)->0
 	uz::Function = (x,y,z,t)->0
 end
 
+
 Base.getindex(motion::SimpleMotion, p::AbstractRange) = motion
 Base.getindex(motion::SimpleMotion, p::AbstractRange, q::AbstractRange) = motion
 Base.getindex(motion::SimpleMotion, p::AbstractVector) = motion
 
-function get_U(motion::SimpleMotion, x::AbstractVector{T}, y::AbstractVector{T}, z::AbstractVector{T}, t::AbstractVector{T}) where {T<:Real}                      
-    Ns = length(x)
-                        
-    Ux = zeros(Ns,1) .+ motion.ux(x, y, z, t')
-    Uy = zeros(Ns,1) .+ motion.uy(x, y, z, t')
-    Uz = zeros(Ns,1) .+ motion.uz(x, y, z, t')
-
-    Ux = sum(abs.(Ux);dims=2) != zeros(Ns,1) ? Ux : nothing
-    Uy = sum(abs.(Uy);dims=2) != zeros(Ns,1) ? Uy : nothing
-    Uz = sum(abs.(Uz);dims=2) != zeros(Ns,1) ? Uz : nothing
-
-    Ux,Uy,Uz
-end
-
-
-"""
-    motion = initialize_motion(obj.motion, seqd.t)
-"""
-function initialize_motion(motion::SimpleMotion, t::AbstractVector{T}, sim_params::Dict) where {T<:Real}
-    return motion
-end
-
-
-function is_dynamic(motion::SimpleMotion)
-    x = 0:0.01:10
-    y = 0:0.01:10
-    z = 0:0.01:10
-    t = 0:0.01:10
-
-    Ux,Uy,Uz = get_U(motion,x,y,z,t)
-    return reduce(|,([Ux,Uy,Uz] .!== nothing))
-end
-
 
 function get_displacements(motion::SimpleMotion, x::AbstractVector{T}, y::AbstractVector{T}, z::AbstractVector{T}, t::AbstractVector{T}) where {T<:Real}
-	Ux = motion.ux(x, y, z, t')
-    Uy = motion.uy(x, y, z, t')
-    Uz = motion.uz(x, y, z, t')
+	xt = x.+ motion.ux(x, y, z, t')
+    yt = y.+ motion.uy(x, y, z, t')
+    zt = z.+ motion.uz(x, y, z, t')
 
-    Ux, Uy, Uz, nothing
+    xt = size(xt, 2) == 1 ? vec(xt) : xt
+    yt = size(yt, 2) == 1 ? vec(yt) : yt
+    zt = size(zt, 2) == 1 ? vec(zt) : zt
+        
+    xt, yt, zt, nothing
 end
