daa398b9aec38b176d6483403f0cd138f2f0b331
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-02-07 17:48:15 +0100

    Improve Rotation Simple Motion Type

--- KomaMRIBase/src/datatypes/phantom/motion/simplemotion/Rotation.jl
+++ KomaMRIBase/src/datatypes/phantom/motion/simplemotion/Rotation.jl
@@ -12,16 +12,26 @@ Parameters:
     f::T                        # Angular velocity [Hz]
 end
 
-function get_simple_motion(rot::Rotation)
-    # Calculate rotation matrix
-    R(t) =  axis_angle(rot.axis, 2π*rot.f*t)
+function SimpleMotion(rot::Rotation)
+    # Rotation matrix
+    R(t) = axis_angle.(Ref(rot.axis), 2π*rot.f*t)
+
+    # Displace using the rotation point
     displace(x,y,z) =  [[x,y,z][i] .- rot.point[i] for i in 1:length(rot.point)]
 
-    ux(x,y,z,t) = begin
+    # Conversion of x,y,z into a (3 x Ns) matrix
+    matrix(x,y,z) = hcat(x,y,z)'
+
+    rotation(x,y,z,t) = begin
         xd, yd, zd = displace(x,y,z)
-        xr = hcat([(R(ti)[1,1]*xd + R(ti)[1,2]*yd + R(ti)[1,3]*zd) .+ rot.point[1] - x for ti in t]...)
-        return xr
+        xyz = matrix(xd,yd,zd)
+        display(R(t))
+        return cat(map(x -> x*xyz, R(t))...,dims=3)
     end
 
-    return SimpleMotion(ux=ux)
+    ux(x,y,z,t) = @view(rotation(x,y,z,t)[1,:,:]) .+ (rot.point[1] .- x)  
+    uy(x,y,z,t) = @view(rotation(x,y,z,t)[2,:,:]) .+ (rot.point[2] .- y)  
+    uz(x,y,z,t) = @view(rotation(x,y,z,t)[3,:,:]) .+ (rot.point[3] .- z)  
+
+    return SimpleMotion(type=rot,ux=ux,uy=uy,uz=uz)
 end
