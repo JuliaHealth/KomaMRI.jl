07be38c7565745d75588ece03a2c1078b5c0920e
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-02-07 12:25:49 +0100

    Test

--- nvtx_test.jl
+++ nvtx_test.jl
@@ -7,28 +7,7 @@ KomaMRICore.CUDA.allowscalar() = false;
 
 phantom = brain_phantom2D()[1:100]
 
-if ARGS[1] == "simple"
-    # Simple Motion
-    print("Simple Motion\n")
-    phantom.motion = SimpleMotion(
-        ux = (x,y,z,t)->sin.(t),
-        uy = (x,y,z,t)->sin.(t),
-        uz = (x,y,z,t)->0
-    )
-elseif ARGS[1] == "arbitrary"
-    # ArbitraryMotion
-    print("Arbitrary Motion\n")
-    K = 10
-    Ns = length(phantom.x)
-    phantom.motion = ArbitraryMotion(
-        [1.0],
-        K,
-        rand(Ns, K-1),
-        rand(Ns, K-1),
-        zeros(Ns, K-1),
-        Bool.(zeros(Ns, K-1))
-    )
-end
+phantom.ux = (x,y,z,t) -> sin.(2π*t)
 
 sys = Scanner()
 
