79e3f5fe1f71eacfa14c4bb727ceed4f9db9ca8a
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-03-04 11:55:52 +0100

    Adapt Rotation motion to new functions (not working yet)

--- KomaMRIBase/src/datatypes/phantom/motion/simplemotion/Rotation.jl
+++ KomaMRIBase/src/datatypes/phantom/motion/simplemotion/Rotation.jl
@@ -6,34 +6,28 @@ Parameters:
 - Angular velocity
 """
 
-mutable struct Rotation{T<:Real} <: SimpleMotionType
-    axis::AbstractVector{T}     # Rotation axis vector
-    point::AbstractVector{T}    # Rotation axis point
-    f::T                        # Angular velocity [Hz]
+@with_kw mutable struct Rotation{T<:Real} <: SimpleMotionType{T}
+    axis::AbstractVector{T}  = [.0, .0, 1]      # Rotation axis vector
+    point::AbstractVector{T} = zeros(3)         # Rotation axis point
+    f::T = 1.0                                  # Angular velocity [Hz]
 end
 
-function SimpleMotion(type::Rotation)
-    # Rotation matrix
-    # R(t) = axis_angle.(Ref(type.axis), 2π*type.f*t)
-    R(t) = [axis_angle(type.axis,x) for x in 2π*type.f*t] # Scalar indexing. Solve
+# Rotation matrix
+R(axis, f, t) = Un.(2π*f*t, Ref(axis))
 
-    # Displace using the rotation point
-    displace(x,y,z) =  [[x,y,z][i] .- type.point[i] for i in 1:length(type.point)]
+# Displace using the rotation point
+displace(x, y, z, point) =  [[x,y,z][i] .- point[i] for i in 1:length(point)]
 
-    # Conversion of x,y,z into a (3 x Ns) matrix
-    matrix(x,y,z) = hcat(x,y,z)'
+# Conversion of x,y,z into a (3 x Ns) matrix
+matrix(x,y,z) = hcat(x,y,z)'
 
-    rotation(x,y,z,t) = begin
-        xd, yd, zd = displace(x,y,z)
-        xyz = matrix(xd,yd,zd)
-        display(R(t))
-        rotated = map(x -> x*xyz, R(t))
-        return cat(rotated...,dims=3)
-    end
-
-    ux(x,y,z,t) = @view(rotation(x,y,z,t)[1,:,:]) .+ (type.point[1] .- x)  
-    uy(x,y,z,t) = @view(rotation(x,y,z,t)[2,:,:]) .+ (type.point[2] .- y)  
-    uz(x,y,z,t) = @view(rotation(x,y,z,t)[3,:,:]) .+ (type.point[3] .- z)  
-
-    return SimpleMotion(type,ux,uy,uz)
+rotation(x, y, z, t, motion_type::Rotation) = begin
+    xd, yd, zd = displace(x, y, z, motion_type.point)
+    xyz = matrix(xd, yd, zd)
+    rotated = map(x -> x*xyz, R(motion_type.axis, motion_type.f, t))
+    return cat(rotated...,dims=3)
 end
+
+displacement_x(motion_type::Rotation{T}, x::AbstractVector{T}, y::AbstractVector{T}, z::AbstractVector{T}, t::AbstractArray{T}) where {T<:Real} = rotation(x,y,z,t,motion_type)[1,:,:] .+ (motion_type.point[1] .- x) 
+displacement_y(motion_type::Rotation{T}, x::AbstractVector{T}, y::AbstractVector{T}, z::AbstractVector{T}, t::AbstractArray{T}) where {T<:Real} = rotation(x,y,z,t,motion_type)[2,:,:] .+ (motion_type.point[2] .- y) 
+displacement_z(motion_type::Rotation{T}, x::AbstractVector{T}, y::AbstractVector{T}, z::AbstractVector{T}, t::AbstractArray{T}) where {T<:Real} = rotation(x,y,z,t,motion_type)[3,:,:] .+ (motion_type.point[3] .- z) 
