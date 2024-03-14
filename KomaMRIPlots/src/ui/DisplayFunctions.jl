dcaf6009cd05fc144401ca69d9abb060ad270b32
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-03-14 13:28:48 +0100

    Delete `plot_cine` function

--- KomaMRIPlots/src/ui/DisplayFunctions.jl
+++ KomaMRIPlots/src/ui/DisplayFunctions.jl
@@ -560,26 +560,6 @@ function plot_image(
 	return plot_koma(p, l; config)
 end
 
-"""
-"""
-function plot_cine(frames, fps; Δt=1/fps)
-
-	x = 0:size(frames[1])[2]-1
-	y = 1:size(frames[1])[1]
-
-	t = 0
-
-	anim = @animate for image in frames
-		t += Δt
-		Plots.plot!(Plots.heatmap(x,y,image',color=:greys; aspect_ratio=:equal),
-					title="Reconstruction (t="*Printf.@sprintf("%.2f", t)*"s)", 
-					xlims=(minimum(x), maximum(x)), 
-					ylims=(minimum(y), maximum(y)))
-	end
-
-	gif(anim, string(@__DIR__)*"/../../../others/cine_recon.gif", fps = fps)
-end
-
 
 """
     p = plot_kspace(seq::Sequence; width=nothing, height=nothing, darkmode=false)
