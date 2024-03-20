94220ec5230cd2e794a171c149173f244e96ceae
beorostica-asusFX505GT-win <beorostica@uc.cl>
2024-03-20 11:00:49 -0300

    Polish block_pulse PulseDesigner function

--- KomaMRIBase/ext/PulseDesignerUnitfulExt.jl
+++ KomaMRIBase/ext/PulseDesignerUnitfulExt.jl
@@ -7,19 +7,15 @@ Angle{T} = Union{Quantity{T, NoDims, typeof(u"rad")}, Quantity{T, NoDims, typeof
 function PulseDesigner.block_pulse(flip_angle::Angle, duration::Unitful.Time;
     phase_offset::Angle=0u"°", freq_offset::Unitful.Frequency=0u"Hz", delay::Unitful.Time=0u"s", sys=Scanner())
     flip_angle, duration, phase_offset, freq_offset, delay = upreferred.((flip_angle, duration, phase_offset, freq_offset, delay)) .|> ustrip
-    return PulseDesigner.block_pulse(FlipAngle(flip_angle), Duration(duration); phase_offset, freq_offset, delay, sys)
+    flip_angle, duration = FlipAngle(flip_angle), Duration(duration)
+    return PulseDesigner.block_pulse(flip_angle, duration; phase_offset, freq_offset, delay, sys)
 end
 
 function PulseDesigner.block_pulse(flip_angle::Angle, bandwidth::Unitful.Frequency;
-    phase_offset::Angle=0u"°", freq_offset::Unitful.Frequency=0u"Hz", delay::Unitful.Time=0u"s", sys=Scanner())
-    flip_angle, bandwidth, phase_offset, freq_offset, delay = upreferred.((flip_angle, bandwidth, phase_offset, freq_offset, delay)) .|> ustrip
-    return PulseDesigner.block_pulse(FlipAngle(flip_angle), Bandwidth(bandwidth); phase_offset, freq_offset, delay, sys)
-end
-
-function PulseDesigner.block_pulse(flip_angle::Angle, bandwidth::Unitful.Frequency, time_bw_product::DimensionlessQuantity;
-    phase_offset::Angle=0u"°", freq_offset::Unitful.Frequency=0u"Hz", delay=0u"s", sys=Scanner())
+    time_bw_product::DimensionlessQuantity=0.25u"Hz*s", phase_offset::Angle=0u"°", freq_offset::Unitful.Frequency=0u"Hz", delay::Unitful.Time=0u"s", sys=Scanner())
     flip_angle, bandwidth, time_bw_product, phase_offset, freq_offset, delay = upreferred.((flip_angle, bandwidth, time_bw_product, phase_offset, freq_offset, delay)) .|> ustrip
-    return PulseDesigner.block_pulse(FlipAngle(flip_angle), Bandwidth(bandwidth), TimeBwProduct(time_bw_product); phase_offset, freq_offset, delay, sys)
+    flip_angle, bandwidth = FlipAngle(flip_angle), Bandwidth(bandwidth)
+    return PulseDesigner.block_pulse(flip_angle, bandwidth; time_bw_product, phase_offset, freq_offset, delay, sys)
 end
 
 end
--- KomaMRIBase/src/sequences/RF/block_pulse.jl
+++ KomaMRIBase/src/sequences/RF/block_pulse.jl
@@ -4,25 +4,16 @@ function block_pulse(flip_angle::FlipAngle, duration::Duration;
 
     flip_angle, duration = flip_angle.val, duration.val
     amplitude = flip_angle / (2π * γ * duration) * exp(im * phase_offset)
-    dead_time = sys.RF_dead_time_T
-    ring_down_time = sys.RF_ring_down_T
-    delay = max(delay, dead_time)
-    block_duration = delay + duration + ring_down_time
+    delay = max(delay, sys.RF_dead_time_T)
+    block_duration = delay + duration + sys.RF_ring_down_T
 
     rf = RF(amplitude, duration, freq_offset, delay)
     return Sequence([Grad(0, 0);;], [rf;;], [ADC(0, 0)], block_duration)
 end
 
-function block_pulse(flip_angle::FlipAngle, bandwidth::Bandwidth;
-    phase_offset=0, freq_offset=0, delay=0, sys=Scanner())
-    duration = 1 / (4 * bandwidth.val)
-    return block_pulse(flip_angle, Duration(duration); phase_offset, freq_offset, delay, sys)
-end
-
-function block_pulse(flip_angle::FlipAngle, bandwidth::Bandwidth, time_bw_product::TimeBwProduct;
-    phase_offset=0, freq_offset=0, delay=0, sys=Scanner())
-    duration = time_bw_product.val / bandwidth.val
-    return block_pulse(flip_angle, Duration(duration); phase_offset, freq_offset, delay, sys)
+function block_pulse(flip_angle::FlipAngle, bandwidth::Bandwidth; time_bw_product=0.25, kwargs...)
+    duration = Duration(time_bw_product / bandwidth.val)
+    return block_pulse(flip_angle, duration; kwargs...)
 end
 
 export block_pulse
