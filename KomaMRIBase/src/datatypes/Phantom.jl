752a9606ca91ee56b49e9c19b6162c4ee4e2f596
curtcorum <1796102+curtcorum@users.noreply.github.com>
2024-03-27 19:08:09 -0500

    Tabs replaced with 4-spaces for consistent indentation. modified:   KomaMRIBase/src/datatypes/Phantom.jl

--- KomaMRIBase/src/datatypes/Phantom.jl
+++ KomaMRIBase/src/datatypes/Phantom.jl
@@ -33,25 +33,25 @@ julia> obj.ρ
 """
  @with_kw mutable struct Phantom{T<:Real}
     name::String = "spins"
-	x::AbstractVector{T}
-	y::AbstractVector{T} = zeros(size(x))
-	z::AbstractVector{T} = zeros(size(x))
-	ρ::AbstractVector{T} = ones(size(x))
-	T1::AbstractVector{T} = ones(size(x)) * 1_000_000
-	T2::AbstractVector{T} = ones(size(x)) * 1_000_000
-	T2s::AbstractVector{T} = ones(size(x)) * 1_000_000
-	#Off-resonance related
-	Δw::AbstractVector{T} = zeros(size(x))
-	#χ::Vector{SusceptibilityModel}
-	#Diffusion
-	Dλ1::AbstractVector{T} = zeros(size(x))
-	Dλ2::AbstractVector{T} = zeros(size(x))
-	Dθ::AbstractVector{T} =  zeros(size(x))
-	#Diff::Vector{DiffusionModel}  #Diffusion map
-	#Motion
-	ux::Function = (x,y,z,t)->0
-	uy::Function = (x,y,z,t)->0
-	uz::Function = (x,y,z,t)->0
+    x::AbstractVector{T}
+    y::AbstractVector{T} = zeros(size(x))
+    z::AbstractVector{T} = zeros(size(x))
+    ρ::AbstractVector{T} = ones(size(x))
+    T1::AbstractVector{T} = ones(size(x)) * 1_000_000
+    T2::AbstractVector{T} = ones(size(x)) * 1_000_000
+    T2s::AbstractVector{T} = ones(size(x)) * 1_000_000
+    #Off-resonance related
+    Δw::AbstractVector{T} = zeros(size(x))
+    #χ::Vector{SusceptibilityModel}
+    #Diffusion
+    Dλ1::AbstractVector{T} = zeros(size(x))
+    Dλ2::AbstractVector{T} = zeros(size(x))
+    Dθ::AbstractVector{T} =  zeros(size(x))
+    #Diff::Vector{DiffusionModel}  #Diffusion map
+    #Motion
+    ux::Function = (x,y,z,t)->0
+    uy::Function = (x,y,z,t)->0
+    uz::Function = (x,y,z,t)->0
 end
 
 """Size and length of a phantom"""
@@ -82,90 +82,90 @@ end
 Separate object spins in a sub-group
 """
 Base.getindex(obj::Phantom, p::AbstractRange) = begin
-	Phantom(name=obj.name,
-			x=obj.x[p],
-			y=obj.y[p],
-			z=obj.z[p],
-			ρ=obj.ρ[p],
-			T1=obj.T1[p],
-			T2=obj.T2[p],
-			T2s=obj.T2s[p],
-			Δw=obj.Δw[p],
-			#Diff=obj.Diff[p], #TODO!
-			Dλ1=obj.Dλ1[p],
-			Dλ2=obj.Dλ2[p],
-			Dθ=obj.Dθ[p],
-			#Χ=obj.Χ[p], #TODO!
-			ux=obj.ux,
-			uy=obj.uy,
-			uz=obj.uz
-			)
+    Phantom(name=obj.name,
+            x=obj.x[p],
+            y=obj.y[p],
+            z=obj.z[p],
+            ρ=obj.ρ[p],
+            T1=obj.T1[p],
+            T2=obj.T2[p],
+            T2s=obj.T2s[p],
+            Δw=obj.Δw[p],
+            #Diff=obj.Diff[p], #TODO!
+            Dλ1=obj.Dλ1[p],
+            Dλ2=obj.Dλ2[p],
+            Dθ=obj.Dθ[p],
+            #Χ=obj.Χ[p], #TODO!
+            ux=obj.ux,
+            uy=obj.uy,
+            uz=obj.uz
+            )
 end
 
 """Separate object spins in a sub-group (lightweigth)."""
 Base.view(obj::Phantom, p::AbstractRange) = begin
-	@views Phantom(name=obj.name,
-			x=obj.x[p],
-			y=obj.y[p],
-			z=obj.z[p],
-			ρ=obj.ρ[p],
-			T1=obj.T1[p],
-			T2=obj.T2[p],
-			T2s=obj.T2s[p],
-			Δw=obj.Δw[p],
-			#Diff=obj.Diff[p], #TODO!
-			Dλ1=obj.Dλ1[p],
-			Dλ2=obj.Dλ2[p],
-			Dθ=obj.Dθ[p],
-			#Χ=obj.Χ[p], #TODO!
-			ux=obj.ux,
-			uy=obj.uy,
-			uz=obj.uz
-			)
+    @views Phantom(name=obj.name,
+            x=obj.x[p],
+            y=obj.y[p],
+            z=obj.z[p],
+            ρ=obj.ρ[p],
+            T1=obj.T1[p],
+            T2=obj.T2[p],
+            T2s=obj.T2s[p],
+            Δw=obj.Δw[p],
+            #Diff=obj.Diff[p], #TODO!
+            Dλ1=obj.Dλ1[p],
+            Dλ2=obj.Dλ2[p],
+            Dθ=obj.Dθ[p],
+            #Χ=obj.Χ[p], #TODO!
+            ux=obj.ux,
+            uy=obj.uy,
+            uz=obj.uz
+            )
 end
 
 """Addition of phantoms"""
 +(s1::Phantom,s2::Phantom) = begin
-	Phantom(name=s1.name*"+"*s2.name,
-		x=[s1.x;s2.x],
-		y=[s1.y;s2.y],
-		z=[s1.z;s2.z],
-		ρ=[s1.ρ;s2.ρ],
-		T1=[s1.T1;s2.T1],
-		T2=[s1.T2;s2.T2],
-		T2s=[s1.T2s;s2.T2s],
-		Δw=[s1.Δw;s2.Δw],
-		#Diff=obj.Diff[p], #TODO!
-		Dλ1=[s1.Dλ1;s2.Dλ1],
-		Dλ2=[s1.Dλ2;s2.Dλ2],
-		Dθ=[s1.Dθ;s2.Dθ],
-		#Χ=obj.Χ[p], #TODO!
-		ux=s1.ux,
-		uy=s1.uy,
-		uz=s1.uz
-	)
+    Phantom(name=s1.name*"+"*s2.name,
+        x=[s1.x;s2.x],
+        y=[s1.y;s2.y],
+        z=[s1.z;s2.z],
+        ρ=[s1.ρ;s2.ρ],
+        T1=[s1.T1;s2.T1],
+        T2=[s1.T2;s2.T2],
+        T2s=[s1.T2s;s2.T2s],
+        Δw=[s1.Δw;s2.Δw],
+        #Diff=obj.Diff[p], #TODO!
+        Dλ1=[s1.Dλ1;s2.Dλ1],
+        Dλ2=[s1.Dλ2;s2.Dλ2],
+        Dθ=[s1.Dθ;s2.Dθ],
+        #Χ=obj.Χ[p], #TODO!
+        ux=s1.ux,
+        uy=s1.uy,
+        uz=s1.uz
+    )
 end
 
 """Scalar multiplication of a phantom"""
 *(α::Real,obj::Phantom) = begin
-	Phantom(name=obj.name,
-		x=obj.x,
-		y=obj.y,
-		z=obj.z,
-		ρ=α*obj.ρ, #Only affects the proton density
-		T1=obj.T1,
-		T2=obj.T2,
-		T2s=obj.T2s,
-		Δw=obj.Δw,
-		#Diff=obj.Diff[p], #TODO!
-		Dλ1=obj.Dλ1,
-		Dλ2=obj.Dλ2,
-		Dθ=obj.Dθ,
-		#Χ=obj.Χ[p], #TODO!
-		ux=obj.ux,
-		uy=obj.uy,
-		uz=obj.uz
-	)
+    Phantom(name=obj.name,
+        x=obj.x,
+        y=obj.y,
+        z=obj.z,
+        ρ=α*obj.ρ, #Only affects the proton density
+        T1=obj.T1,
+        T2=obj.T2,
+        T2s=obj.T2s,
+        Δw=obj.Δw,
+        #Diff=obj.Diff[p], #TODO!
+        Dλ1=obj.Dλ1,
+        Dλ2=obj.Dλ2,
+        Dθ=obj.Dθ,
+        #Χ=obj.Χ[p], #TODO!
+        ux=obj.ux,
+        uy=obj.uy,
+        uz=obj.uz
+    )
 end
 
 """
@@ -282,26 +282,26 @@ function brain_phantom2D(; axis="axial", ss=4, us=1)
         (class.==209)*.77 .+ #FAT2
         (class.==232)*1 .+ #DURA
         (class.==255)*.77 #MARROW
-	Δw_fat = -220*2π
-	Δw = (class.==93)*Δw_fat .+ #FAT1
-		(class.==209)*Δw_fat    #FAT2
-	T1 = T1*1e-3
-	T2 = T2*1e-3
-	T2s = T2s*1e-3
+    Δw_fat = -220*2π
+    Δw = (class.==93)*Δw_fat .+ #FAT1
+        (class.==209)*Δw_fat    #FAT2
+    T1 = T1*1e-3
+    T2 = T2*1e-3
+    T2s = T2s*1e-3
 
     # Define and return the Phantom struct
     obj = Phantom{Float64}(
         name = "brain2D_"*axis,
-		x = y[ρ.!=0],
-		y = x[ρ.!=0],
-		z = 0*x[ρ.!=0],
-		ρ = ρ[ρ.!=0],
-		T1 = T1[ρ.!=0],
-		T2 = T2[ρ.!=0],
-		T2s = T2s[ρ.!=0],
-		Δw = Δw[ρ.!=0],
+        x = y[ρ.!=0],
+        y = x[ρ.!=0],
+        z = 0*x[ρ.!=0],
+        ρ = ρ[ρ.!=0],
+        T1 = T1[ρ.!=0],
+        T2 = T2[ρ.!=0],
+        T2s = T2s[ρ.!=0],
+        Δw = Δw[ρ.!=0],
     )
-	return obj
+    return obj
 end
 
 """
@@ -374,13 +374,13 @@ function brain_phantom3D(;ss=4, us=1, start_end=[160, 200])
     M, N, Z = size(class)
     FOVx = (M-1)*Δx #[m]
     FOVy = (N-1)*Δy #[m]
-	FOVz = (Z-1)*Δz #[m]
+    FOVz = (Z-1)*Δz #[m]
     xx = reshape(-FOVx/2:Δx:FOVx/2,M,1,1) #spin coordinates
     yy = reshape(-FOVy/2:Δy:FOVy/2,1,N,1) #spin coordinates
-	zz = reshape(-FOVz/2:Δz:FOVz/2,1,1,Z) #spin coordinates
+    zz = reshape(-FOVz/2:Δz:FOVz/2,1,1,Z) #spin coordinates
     x = 1*xx .+ 0*yy .+ 0*zz
-	y = 0*xx .+ 1*yy .+ 0*zz
-	z = 0*xx .+ 0*yy .+ 1*zz
+    y = 0*xx .+ 1*yy .+ 0*zz
+    z = 0*xx .+ 0*yy .+ 1*zz
 
     # Define spin property vectors
     T2 = (class.==23)*329 .+ #CSF
@@ -428,26 +428,26 @@ function brain_phantom3D(;ss=4, us=1, start_end=[160, 200])
         (class.==209)*.77 .+ #FAT2
         (class.==232)*1 .+ #DURA
         (class.==255)*.77 #MARROW
-	Δw_fat = -220*2π
-	Δw = (class.==93)*Δw_fat .+ #FAT1
-		(class.==209)*Δw_fat    #FAT2
-	T1 = T1*1e-3
-	T2 = T2*1e-3
-	T2s = T2s*1e-3
+    Δw_fat = -220*2π
+    Δw = (class.==93)*Δw_fat .+ #FAT1
+        (class.==209)*Δw_fat    #FAT2
+    T1 = T1*1e-3
+    T2 = T2*1e-3
+    T2s = T2s*1e-3
 
     # Define and return the Phantom struct
     obj = Phantom{Float64}(
         name = "brain3D",
-		x = y[ρ.!=0],
-		y = x[ρ.!=0],
-		z = z[ρ.!=0],
-		ρ = ρ[ρ.!=0],
-		T1 = T1[ρ.!=0],
-		T2 = T2[ρ.!=0],
-		T2s = T2s[ρ.!=0],
-		Δw = Δw[ρ.!=0],
+        x = y[ρ.!=0],
+        y = x[ρ.!=0],
+        z = z[ρ.!=0],
+        ρ = ρ[ρ.!=0],
+        T1 = T1[ρ.!=0],
+        T2 = T2[ρ.!=0],
+        T2s = T2s[ρ.!=0],
+        Δw = Δw[ρ.!=0],
     )
-	return obj
+    return obj
 end
 
 """
@@ -526,11 +526,11 @@ function pelvis_phantom2D(; ss=4, us=1)
         (class.==153)*80 .+     # SoftTissue
         (class.==204)*52 .+     # SpongyBone
         (class.==255)*.3        # CorticalBone
-	Δw_fat = -220 * 2π
-	Δw = (class.==102) * Δw_fat # FAT1
-	T1 = T1*1e-3
-	T2 = T2*1e-3
-	T2s = T2s*1e-3
+    Δw_fat = -220 * 2π
+    Δw = (class.==102) * Δw_fat # FAT1
+    T1 = T1*1e-3
+    T2 = T2*1e-3
+    T2s = T2s*1e-3
 
     # Define and return the Phantom struct
     obj = Phantom{Float64}(
@@ -544,5 +544,5 @@ function pelvis_phantom2D(; ss=4, us=1)
         T2s = T2s[ρ.!=0],
         Δw = Δw[ρ.!=0],
     )
-	return obj
+    return obj
 end
