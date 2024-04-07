c9677eef5a38a9bb43ed0b81e3d6215d65072032
Carlos Castillo Passi <cncastillo@uc.cl>
2024-04-07 15:44:43 -0400

    KomaMRIBase: Export is_X_on

--- KomaMRIBase/src/KomaMRIBase.jl
+++ KomaMRIBase/src/KomaMRIBase.jl
@@ -38,6 +38,7 @@ export Grad, RF, ADC, Delay
 export dur, get_block_start_times, get_samples
 export DiscreteSequence
 export discretize, get_adc_phase_compensation, get_adc_sampling_times
+export is_Gx_on, is_Gy_on, is_Gz_on, is_RF_on, is_ADC_on
 # This are also used for simulation
 export kfoldperm, trapz, cumtrapz
 # Phantom
