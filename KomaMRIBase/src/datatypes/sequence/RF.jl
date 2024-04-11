807c0c43aedc5f9b1afe98514e8889f64f4bb0ea
beorostica <36004160+beorostica@users.noreply.github.com>
2024-04-11 15:28:26 -0400

    Update KomaMRIBase/src/datatypes/sequence/RF.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- KomaMRIBase/src/datatypes/sequence/RF.jl
+++ KomaMRIBase/src/datatypes/sequence/RF.jl
@@ -101,9 +101,12 @@ end
 For comparing two `RF`s custom types
 """
 Base.isapprox(rf1::RF, rf2::RF) = begin
-    return all(length(getfield(rf1, k)) == length(getfield(rf2, k)) for k ∈ fieldnames(RF)) &&
-        all(size(getfield(rf1, k)) == size(getfield(rf2, k)) for k ∈ fieldnames(RF)) &&
-        all(all(isapprox.(getfield(rf1, k), getfield(rf2, k), atol=1e-9)) for k ∈ fieldnames(RF))
+    return all(length(getfield(rf1, k)) == length(getfield(rf2, k)) for k in fieldnames(RF)) &&
+           all(size(getfield(rf1, k)) == size(getfield(rf2, k)) for k in fieldnames(RF)) &&
+           all(
+               all(isapprox.(getfield(rf1, k), getfield(rf2, k), atol=1e-9)) for
+               k in fieldnames(RF)
+           )
 end
 
 # Properties
