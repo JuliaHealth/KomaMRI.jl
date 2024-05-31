2549b383c3bc9d6294e2cd97db6202d99bb31a59
Ryan Kierulf <ryanakierulf@gmail.com>
2024-05-31 17:18:50 -0500

    Improve error message for float64 unsupported

--- KomaMRICore/src/simulation/SimulatorCore.jl
+++ KomaMRICore/src/simulation/SimulatorCore.jl
@@ -335,7 +335,8 @@ function simulate(
     backend = get_backend(sim_params["gpu"])
     if !KA.supports_float64(backend) && sim_params["precision"] == "f64"
         @info """ Backend: '$(name(backend))' does not support 64-bit precision 
-        floating point operations. Converting to type Float32.
+        floating point operations. Automatically converting to type Float32.
+        (set sim_param["precision"] = "f32" to avoid seeing this message).
         """ maxlog=1
     end
     if sim_params["precision"] == "f64" && KA.supports_float64(backend)
