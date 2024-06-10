d977f2529d4655b6db14978a985659bee9b5745a
curtcorum <1796102+curtcorum@users.noreply.github.com>
2024-06-09 22:22:53 -0500

    A fair amount of work to estimate proper 3D recon parameters from seq, and work and debugging of the scan counters for 3D. modified:   KomaMRICore/src/rawdata/ISMRMRD.jl

--- KomaMRICore/src/rawdata/ISMRMRD.jl
+++ KomaMRICore/src/rawdata/ISMRMRD.jl
@@ -43,7 +43,7 @@ const ISMRMRD_ACQ_USER6                               = 1b64 << ( 62 - 1 )
 const ISMRMRD_ACQ_USER7                               = 1b64 << ( 63 - 1 )
 const ISMRMRD_ACQ_USER8                               = 1b64 << ( 64 - 1 )
 """
-    raw = signal_to_raw_data(signal, seq; phantom_name, sys, sim_params, ndims)
+    raw = signal_to_raw_data(signal, seq; phantom_name, sys, sim_params, ndims=2)
 
 Transforms the raw signal into a RawAcquisitionData struct (nearly equivalent to the ISMRMRD
 format) used for reconstruction with MRIReco.
@@ -78,48 +78,25 @@ julia> plot_signal(raw)
 """
 function signal_to_raw_data(
     signal, seq;
-    phantom_name="Phantom", sys=Scanner(), sim_params=Dict{String,Any}(), ndims=0,
+    phantom_name="Phantom", sys=Scanner(), sim_params=Dict{String,Any}(), ndims=2, use_ndseq=true
 )
-
-    @debug "Something going funny here, making 960 extra profiles for ge3d_cac.seq! *** CAC 240608"
-
     version = string(VersionNumber(Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "..", "Project.toml"))["version"]))
-    #Number of samples and FOV
-    _, ktraj = get_kspace(seq) #kspace information
-    mink = minimum(ktraj, dims=1)
-    maxk = maximum(ktraj, dims=1)
-    Wk = maxk .- mink
-    idxs_zero = findall(iszero, Wk)  #check for zeros
-    Wk[idxs_zero] .= 1e6
-    Δx = 1 ./ Wk[1:3] #[m] x-y-z
-    Nx = get(seq.DEF, "Nx", 1)
-    Ny = get(seq.DEF, "Ny", 1)
-    Nz = get(seq.DEF, "Nz", 1)
-    Ns = get(seq.DEF, "Ns", 1) # number of slices or slabs
-    if haskey(seq.DEF, "FOV")
-        FOVx, FOVy, FOVz = seq.DEF["FOV"] #[m]
-        if FOVx > 1 FOVx *= 1e-3 end #mm to m, older versions of Pulseq saved FOV in mm
-        if FOVy > 1 FOVy *= 1e-3 end #mm to m, older versions of Pulseq saved FOV in mm
-        if FOVz > 1 FOVz *= 1e-3 end #mm to m, older versions of Pulseq saved FOV in mm
-        Nx = round(Int64, FOVx / Δx[1])
-        Ny = round(Int64, FOVy / Δx[2])
-        Nz = round(Int64, FOVy / Δx[3])      
+
+    #Estimate sequence reconstruction dimension, using seq.DEF and potentially other information
+    Nd_seq, Nx, Ny, Nz, Ns, FOVx, FOVy, FOVz, Δx, ktraj = estimate_seq_recon_dimension(signal, seq; sim_params)
+    if use_ndseq
+        if ndims != Nd_seq; @warn("Using estimated dimension of $Nd_seq for .mrd file."); ndims = Nd_seq; end
     else
-        FOVx = Nx * Δx[1]
-        FOVy = Ny * Δx[2]
-        FOVz = Ny * Δx[3]
-    end
-    Nd_seq = (Nx > 1) + (Ny > 1) + (Nz > 1)
-    if ndims < 0 #just in case ndims is provided in a call
         if ndims != Nd_seq; @warn("Seqfile is $Nd_seq dimensional but .mrd file will be $ndims."); end
-    else
-        ndims = Nd_seq
     end
     if ndims == 2
         @info "Creating 2D ISMRMRD file ..."
-    else        
+    elseif ndims == 3        
         @info "Creating 3D ISMRMRD file ..."
-    end  
+    else
+        @warn("Seqfile is $Nd_seq dimensional which is not yet supported.")
+    end
+  
     #It needs to be transposed for the raw data
     ktraj = maximum(2*abs.(ktraj[:])) == 0 ? transpose(ktraj) : transpose(ktraj)./ maximum(2*abs.(ktraj[:]))
 
@@ -152,11 +129,11 @@ function signal_to_raw_data(
         "reconSize"                      => [Nx, Ny, Nz],                       #reconSpace>matrixSize
         "reconFOV"                       => Float32.([FOVx, FOVy, FOVz]*1e3),   #reconSpace>fieldOfView_mm
         #encodingLimits
-        "enc_lim_kspace_encoding_step_0" => Limit(0, Nx-1, ceil(Int, Nx / 2)),#min, max, center, e.g. phase encoding line number
-        "enc_lim_kspace_encoding_step_1" => Limit(0, Ny-1, ceil(Int, Ny / 2)),#min, max, center, e.g. partition encoding number
-        "enc_lim_kspace_encoding_step_2" => Limit(0, Nz-1, ceil(Int, Nz / 2)),#min, max, center, e.g. partition encoding number
+        "enc_lim_kspace_encoding_step_0" => Limit(0, Nx-1, ceil(Int, Nx / 2)),  #min, max, center, e.g. phase encoding line number
+        "enc_lim_kspace_encoding_step_1" => Limit(0, Ny-1, ceil(Int, Ny / 2)),  #min, max, center, e.g. partition encoding number
+        "enc_lim_kspace_encoding_step_2" => Limit(0, Nz-1, ceil(Int, Nz / 2)),  #min, max, center, e.g. partition encoding number
         "enc_lim_average"                => Limit(0, 0, 0),                     #min, max, center, e.g. signal average number
-        "enc_lim_slice"                  => Limit(0, Ns-1, ceil(Int, Ns / 2)),#min, max, center, e.g. imaging slice number
+        "enc_lim_slice"                  => Limit(0, Ns-1, ceil(Int, Ns / 2)),  #min, max, center, e.g. imaging slice number
         "enc_lim_contrast"               => Limit(0, 0, 0),                     #min, max, center, e.g. echo number in multi-echo
         "enc_lim_phase"                  => Limit(0, 0, 0),                     #min, max, center, e.g. cardiac phase number
         "enc_lim_repetition"             => Limit(0, 0, 0),                     #min, max, center, e.g. dynamic number for dynamic scanning
@@ -175,13 +152,15 @@ function signal_to_raw_data(
     #Then, we define the Profiles
     profiles = Profile[]
     t_acq = get_adc_sampling_times(seq)
-    Nadcs = sum(is_ADC_on.(seq))
-    NadcsPerSlice = floor(Int, Nadcs / Ns) +1
-    NadcsPerPE1 = floor(Int, Nadcs / Nz) +1
+    Nro = sum(is_ADC_on.(seq))
+    NroPerPE1   = floor(Int, Nro / (Ny*Ns*Nz))
+    NroPerSlice = floor(Int, Nro / Ns)
+    NroPerPE2   = floor(Int, Nro / Nz)
     scan_counter = 0
-    ns = 0
-    nz = 0
-    current = 1
+    ny = 0 #PE1 counter
+    ns = 0 #Slice counter
+    nz = 0 #PE2 counter
+    current = 1 #used for traj and data partition
     for s = seq #Iterate over sequence blocks
         if is_ADC_on(s)
             Nsamples = s.ADC.N[1]
@@ -191,9 +170,11 @@ function signal_to_raw_data(
             if scan_counter == 0
                 flag += ISMRMRD_ACQ_FIRST_IN_ENCODE_STEP1
                 flag += ISMRMRD_ACQ_FIRST_IN_SLICE
-            elseif scan_counter == Nadcs - 1
-                flag += ISMRMRD_ACQ_LAST_IN_ENCODE_STEP1
-                flag += ISMRMRD_ACQ_LAST_IN_SLICE
+                if Nz > 1; flag += ISMRMRD_ACQ_FIRST_IN_ENCODE_STEP2; end
+            elseif scan_counter == Nro - 1 #Needs fixing? CAC 240609 ***
+                #flag += ISMRMRD_ACQ_LAST_IN_ENCODE_STEP1
+                #flag += ISMRMRD_ACQ_LAST_IN_SLICE
+                #if Nz > 1; flag += ISMRMRD_ACQ_LAST_IN_ENCODE_STEP2; end
             end
             #Header of profile data, head::AcquisitionHeader
             head = AcquisitionHeader(
@@ -219,15 +200,15 @@ function signal_to_raw_data(
                 Float32.((0, 0, 1)), #slice_dir float32x3: Directional cosines of the slice direction
                 Float32.((0, 0, 0)), #patient_table_position float32x3: Patient table off-center
                 EncodingCounters( #idx uint16x17: Encoding loop counters
-                    UInt16(scan_counter), #kspace_encode_step_1 uint16: e.g. phase encoding line number
+                    UInt16(ny), #kspace_encode_step_1 uint16: e.g. phase encoding line number
                     UInt16(nz), #kspace_encode_step_2 uint16: e.g. partition encoding number
-                    UInt16(0), #average uint16: e.g. signal average number
+                    UInt16(0),  #average uint16: e.g. signal average number
                     UInt16(ns), #slice uint16: e.g. imaging slice number
-                    UInt16(0), #contrast uint16: e.g. echo number in multi-echo
-                    UInt16(0), #phase uint16: e.g. cardiac phase number
-                    UInt16(0), #repetition uint16: e.g. dynamic number for dynamic scanning
-                    UInt16(0), #set uint16: e.g. flow encoding set
-                    UInt16(0), #segment uint16: e.g. segment number for segmented acquisition
+                    UInt16(0),  #contrast uint16: e.g. echo number in multi-echo
+                    UInt16(0),  #phase uint16: e.g. cardiac phase number
+                    UInt16(0),  #repetition uint16: e.g. dynamic number for dynamic scanning
+                    UInt16(0),  #set uint16: e.g. flow encoding set
+                    UInt16(0),  #segment uint16: e.g. segment number for segmented acquisition
                     Tuple(UInt16(0) for i=1:8) #user uint16x8: Free user parameters
                 ),
                 Tuple(Int32(0) for i=1:8), #user_int int32x8: Free user parameters
@@ -240,21 +221,92 @@ function signal_to_raw_data(
             #Saving profile
             push!(profiles, Profile(head, Float32.(traj), ComplexF32.(dat)))
             #Update counters
-            scan_counter += 1
+            scan_counter += 1 #another ro
             current += Nsamples
-            if scan_counter % NadcsPerSlice == 0
+            if  scan_counter % NroPerSlice == 0
                 ns += 1 #another slice
-                scan_counter = 0 #reset counter
             end
-            if scan_counter % NadcsPerPE1 == 0 #Using Ns for slice number
+            if scan_counter % NroPerPE1 == 0
+                ny += 1 #another kspace_encode_step_2
+            end
+            if scan_counter % NroPerPE2 == 0
+                ny = 0
                 nz += 1 #another kspace_encode_step_2
-                scan_counter = 0 #reset counter
             end
             # more here for other counters, i.e. dynamic
         end
     end
 
     return RawAcquisitionData(params, profiles)
+end
+
+"""
+    Nd_seq, Nx, Ny, Nz, Ns, FOVx, FOVy, FOVz, Δx = estimate_seq_recon_dimension(signal, seq; sim_params)
+
+Utility function for the best estimate of the reconstruction dimension.
+
+# Arguments
+- `signal`: (`::Matrix{Complex}`) raw signal matrix
+- `seq`: (`::Sequence`) Sequence struct
+
+# Keywords
+- `sim_params`: (`::Dict{String, Any}`, `=Dict{String,Any}()`) simulation parameter dictionary
+
+# Returns
+- `Nd_seq`: (`Int`) Estimated reconstruction dimension of seq.
+
+# Examples
+```julia-repl
+julia> seq_file = joinpath(dirname(pathof(KomaMRI)), "../examples/1.sequences/epi_se.seq")
+
+julia> sys, obj, seq = Scanner(), brain_phantom2D(), read_seq(seq_file)
+
+julia> sim_params = KomaMRICore.default_sim_params(); sim_params["return_type"] = "mat"
+
+julia> signal = simulate(obj, seq, sys; sim_params)
+
+julia> Nd_seq = estimate_seq_recon_dimension(seq, signal; sim_params)
+
+julia> OUTPUT ****
+```
+"""
+function estimate_seq_recon_dimension(
+    signal, seq; sim_params=Dict{String,Any}(),
+)
+
+    #Number of samples and FOV
+    _, ktraj = get_kspace(seq) #kspace information
+    mink = minimum(ktraj, dims=1)
+    maxk = maximum(ktraj, dims=1)
+    Wk = maxk .- mink
+    idxs_zero = findall(iszero, Wk)  #check for zeros
+    Wk[idxs_zero] .= 1e6
+    Δx = 1 ./ Wk[1:3] #[m] x-y-z
+    Nx = ceil(Int64, get(seq.DEF, "Nx", 1))
+    Ny = ceil(Int64, get(seq.DEF, "Ny", 1))
+    Nz = ceil(Int64, get(seq.DEF, "Nz", 1))
+    Ns = ceil(Int64, get(seq.DEF, "Ns", 1)) # number of slices or slabs
+    if haskey(seq.DEF, "FOV")
+        FOVx, FOVy, FOVz = seq.DEF["FOV"] #[m]
+        if FOVx > 1 FOVx *= 1e-3 end #mm to m, older versions of Pulseq saved FOV in mm
+        if FOVy > 1 FOVy *= 1e-3 end #mm to m, older versions of Pulseq saved FOV in mm
+        if FOVz > 1 FOVz *= 1e-3 end #mm to m, older versions of Pulseq saved FOV in mm
+        if Nx == 1 Nx = ceil(Int64, FOVx / Δx[1]) end
+        if Ny == 1 Ny = ceil(Int64, FOVy / Δx[2]) end
+        if Nz == 1 Nz = ceil(Int64, FOVy / Δx[3]) end      
+    else
+        @warn "Estimating FOV parameters from seq.DEF Nx, Ny, Nz and traj."
+        FOVx = Nx * Δx[1]
+        FOVy = Ny * Δx[2]
+        FOVz = Ny * Δx[3]
+    end
+    Nd_seq = (Nx > 1) + (Ny > 1) + (Nz > 1)
+    s_ktraj = size( ktraj)
+    @debug "Nd_seq = $Nd_seq"
+    @debug "Nx, Ny, Nz, Ns = $Nx, $Ny, $Nz, $Ns"
+    @debug "FOVx, FOVy, FOVz = $FOVx, $FOVy, $FOVz"
+    @debug "Δx, size( ktraj) = $Δx, $s_ktraj"
+    return Nd_seq, Nx, Ny, Nz, Ns, FOVx, FOVy, FOVz, Δx, ktraj
 end
 
 Base.show(io::IO, raw::RawAcquisitionData) = begin
