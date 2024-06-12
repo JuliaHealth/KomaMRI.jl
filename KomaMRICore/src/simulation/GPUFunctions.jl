cba4a2474738e5d42247d8fef656de7d631c0119
Ryan Kierulf <ryanakierulf@gmail.com>
2024-06-12 14:31:18 -0500

    Reformat docstring

--- KomaMRICore/src/simulation/GPUFunctions.jl
+++ KomaMRICore/src/simulation/GPUFunctions.jl
@@ -81,12 +81,11 @@ end
 """
     print_devices()
 
-# Arguments
-- 'use_gpu':  ('::Bool') If true, check for loaded / functional GPU backends and print
-appropriate warnings if no GPU backends have been loaded
-
 Simple function to print available devices. Calls internal get_backend() function to
 get the appropriate GPU / CPU backend and prints device information.
+
+# Arguments
+- 'use_gpu':  ('::Bool') If true, check for loaded / functional GPU backends and print appropriate warnings if no GPU backends have been loaded
 """
 function print_devices(use_gpu = true)
     backend = get_backend(use_gpu)
