4d4107195adc824114522799824ad98cc4b31fba
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-06-15 12:02:35 +0200

    Delete adapt for ArbitraryMotion

--- KomaMRICore/ext/KomaoneAPIExt.jl
+++ KomaMRICore/ext/KomaoneAPIExt.jl
@@ -9,12 +9,6 @@ KomaMRICore.isfunctional(::oneAPIBackend) = oneAPI.functional()
 KomaMRICore.set_device!(::oneAPIBackend, val) = oneAPI.device!(val)
 KomaMRICore.device_name(::oneAPIBackend) = oneAPI.properties(oneAPI.device()).name
 
-function Adapt.adapt_storage(
-    ::oneAPIBackend, x::Vector{KomaMRICore.LinearInterpolator{T,V}}
-) where {T<:Real,V<:AbstractVector{T}}
-    return Adapt.adapt.(oneArray, a)
-end
-
 function KomaMRICore._print_devices(::oneAPIBackend)
     devices = [
         Symbol("($(i-1)$(i == 1 ? "*" : " "))") => oneAPI.properties(d).name for
