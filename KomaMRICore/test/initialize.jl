b7f30b1a6ceaaa0b0cf495ed4b920c253bba5ede
Ryan Kierulf <ryanakierulf@gmail.com>
2024-06-18 11:56:31 -0500

    Avoid installing all GPU backends

--- KomaMRICore/test/initialize.jl
+++ KomaMRICore/test/initialize.jl
@@ -1,16 +1,23 @@
+using Pkg
+using Suppressor
+
 const USE_GPU = if "AMDGPU" in ARGS
+    @suppress Pkg.add("AMDGPU")
     using AMDGPU
     @info "Testing AMD" maxlog=1
     true
 elseif "CUDA" in ARGS
+    @suppress Pkg.add("CUDA")
     using CUDA
     @info "Testing CUDA" maxlog=1
     true
 elseif "Metal" in ARGS
+    @suppress Pkg.add("Metal")
     using Metal
     @info "Testing Metal" maxlog=1
     true
 elseif "oneAPI" in ARGS
+    @suppress Pkg.add("oneAPI")
     using oneAPI
     @info "Testing oneAPI" maxlog=1
     true
