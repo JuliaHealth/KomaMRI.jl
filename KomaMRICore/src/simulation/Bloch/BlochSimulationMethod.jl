ef5bfc71b60071bbab20df7b95936f593a17e9e5
Ryan Kierulf <ryanakierulf@gmail.com>
2024-06-18 15:29:50 -0500

    Add some more temporary printing

--- KomaMRICore/src/simulation/Bloch/BlochSimulationMethod.jl
+++ KomaMRICore/src/simulation/Bloch/BlochSimulationMethod.jl
@@ -65,6 +65,7 @@ function run_spin_precession!(
     exp_argument1 = one_im .* ϕ
     exp_argument2 = - tp' ./ p.T2
     exp_argument = exp_argument1 .+ exp_argument2
+    println(typeof(exp_argument))
     exp_result = exp.(exp_argument)
     elementwise_multiplication = M.xy .* exp_result
     Mxy = [M.xy elementwise_multiplication]
