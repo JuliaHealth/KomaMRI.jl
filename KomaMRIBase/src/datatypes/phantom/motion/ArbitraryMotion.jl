4df277b9dfa8bbca3301593019ab0e3a2b48a268
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-06-19 20:08:16 +0200

    Fix rangeT, but use LinRange

--- KomaMRIBase/src/datatypes/phantom/motion/ArbitraryMotion.jl
+++ KomaMRIBase/src/datatypes/phantom/motion/ArbitraryMotion.jl
@@ -93,12 +93,13 @@ function GriddedInterpolation(nodes, A, ITP)
 end
 
 function rangeT(start::T, stop::T, length::Int) where {T<:Real}
-    return StepRangeLen{T,T,T}(T(start), T(stop), length)
+    r = range(start, stop, length)
+    return StepRangeLen{T,T,T}(start, T(r.step), length)
 end
 
 function interpolate(motion::ArbitraryMotion{T}, Ns::Val{1}) where {T<:Real}
     _, Nt = size(motion.dx)
-    t = rangeT(zero(T), oneunit(T), Nt)
+    t = LinRange(zero(T), oneunit(T), Nt)
     itpx = GriddedInterpolation((t, ), motion.dx[:], (Gridded(Linear()), ))
     itpy = GriddedInterpolation((t, ), motion.dy[:], (Gridded(Linear()), ))
     itpz = GriddedInterpolation((t, ), motion.dz[:], (Gridded(Linear()), ))
@@ -107,8 +108,8 @@ end
 
 function interpolate(motion::ArbitraryMotion{T}, Ns::Val) where {T<:Real}
     Ns, Nt = size(motion.dx)
-    id = rangeT(oneunit(T), T(Ns), Ns)
-    t  = rangeT(zero(T), oneunit(T), Nt)
+    id = LinRange(oneunit(T), T(Ns), Ns)
+    t  = LinRange(zero(T), oneunit(T), Nt)
     itpx = GriddedInterpolation((id, t), motion.dx, (NoInterp(), Gridded(Linear())))
     itpy = GriddedInterpolation((id, t), motion.dy, (NoInterp(), Gridded(Linear())))
     itpz = GriddedInterpolation((id, t), motion.dz, (NoInterp(), Gridded(Linear())))
