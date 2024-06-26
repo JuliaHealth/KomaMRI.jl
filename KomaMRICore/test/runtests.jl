2ea2d984aced5de0ae8fa2f5a0e8134b90545723
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-06-26 12:24:16 +0200

    Try vectors with the simplest reproducible situation

--- KomaMRICore/test/runtests.jl
+++ KomaMRICore/test/runtests.jl
@@ -327,13 +327,12 @@ end
     # @test NMRSE(sig, sig_jemris) < 1 #NMRSE < 1%
 
     N = 5
-    x = range(0f0, 1f0, N); x = StepRangeLen{Float32,Float32,Float32}(0f0, Float32(x.step), N)
+    x = collect(range(0f0, 1f0, N)) 
     y = rand(Float32, N)
 
     itp = KomaMRIBase.Interpolations.interpolate((x,), y, KomaMRIBase.Interpolations.Gridded(KomaMRIBase.Interpolations.Linear()))
     cuitp = itp |> gpu;
 
-    # ITP Call
     xp = rand(Float32, N); xp = xp |> gpu;
 
     u = cuitp.(xp)
