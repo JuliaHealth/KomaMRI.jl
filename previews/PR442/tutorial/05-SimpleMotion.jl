88c0920e43e5e51f19cee2fe49552df63c82deed
Documenter.jl <documenter@juliadocs.github.io>
2024-08-24 16:04:02 +0000

    build based on 93c5c61

--- previews/PR442/tutorial/05-SimpleMotion.jl
+++ previews/PR442/tutorial/05-SimpleMotion.jl
@@ -5,7 +5,7 @@ obj = brain_phantom2D()
 obj.Δw .= 0 # hide
 
 obj.motion = MotionList(
-    Translation(time=TimeRange(t_start=0.0, t_end=200e-3), dx=2e-2, dy=0.0, dz=0.0)
+    Translate(2e-2, 0.0, 0.0, TimeRange(t_start=0.0, t_end=200e-3))
 )
 p1 = plot_phantom_map(obj, :T2 ; height=450, intermediate_time_samples=4) # hide
 
