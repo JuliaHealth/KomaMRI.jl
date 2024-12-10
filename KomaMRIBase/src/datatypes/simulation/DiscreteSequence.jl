df42cfdcb4299aea8d9ac3991ecdcea73bbb9efa
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-12-10 17:19:31 +0100

    Remove `unique!` call in `discretize`

--- KomaMRIBase/src/datatypes/simulation/DiscreteSequence.jl
+++ KomaMRIBase/src/datatypes/simulation/DiscreteSequence.jl
@@ -87,7 +87,7 @@ based on simulation parameters.
 """
 function discretize(seq::Sequence; sampling_params=default_sampling_params(), add_times=[])
     t, Δt      = get_variable_times(seq; Δt=sampling_params["Δt"], Δt_rf=sampling_params["Δt_rf"])
-    sort!(unique!(append!(t, add_times)))
+    sort!(append!(t, add_times))
     B1, Δf     = get_rfs(seq, t)
     Gx, Gy, Gz = get_grads(seq, t)
     tadc       = get_adc_sampling_times(seq)
