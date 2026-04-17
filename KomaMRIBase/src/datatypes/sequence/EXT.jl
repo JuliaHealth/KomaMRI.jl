f5be935bcc19c86cabecd4401da6245dc3b96579
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2026-04-17 17:54:11 +0200

    Include extension comparison in `≈` and improve code style

--- KomaMRIBase/src/datatypes/sequence/EXT.jl
+++ KomaMRIBase/src/datatypes/sequence/EXT.jl
@@ -10,7 +10,10 @@ include("extensions/Trigger.jl")
 get_EXT_type_from_symbol(::Val)  = nothing
 get_symbol_from_EXT_type(::Type) = nothing
 
-Base.:(==)(x::Extension, y::Extension) = (typeof(x) == typeof(y)) && all([getfield(x, k) == getfield(y, k) for k in fieldnames(typeof(x))])
+function Base.:(==)(x::Extension, y::Extension)
+    return (typeof(x) == typeof(y)) && all([getfield(x, k) == getfield(y, k) for k in fieldnames(typeof(x))])
+end
+
 function Base.:(≈)(x::Extension, y::Extension)
     typeof(x) == typeof(y) || return false
     equal = true
