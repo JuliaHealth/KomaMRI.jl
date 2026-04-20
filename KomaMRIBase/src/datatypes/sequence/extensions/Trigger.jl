aac0ec6dfaeaeefe1416a4fb213bc22a9de1b9d8
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2026-04-21 01:45:08 +0200

    Commit of the following: - Fix Trigger output format (Int) - (Cursor) Output format improvements

--- KomaMRIBase/src/datatypes/sequence/extensions/Trigger.jl
+++ KomaMRIBase/src/datatypes/sequence/extensions/Trigger.jl
@@ -6,7 +6,7 @@ mutable struct Trigger <: Extension
 end
 
 get_scale(::Type{Trigger}) = [1 1 1e-6 1e-6]
-get_scanf_format(::Type{Trigger}) = "%i %i %f %f"
+get_scanf_format(::Type{Trigger}) = "%i %i %i %i"
 get_EXT_type_from_symbol(::Val{:TRIGGERS}) = Trigger
 get_symbol_from_EXT_type(::Type{Trigger})  = "TRIGGERS"
 extension_type_header(::Type{Trigger}) = "# Extension specification for triggers:\n# id type channel d1 [us] d2 [us]\n"
