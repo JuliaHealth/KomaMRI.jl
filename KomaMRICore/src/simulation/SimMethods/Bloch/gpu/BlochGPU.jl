77e1b4e48a6fbda340df3983217b10f4b05a06ea
Kareem Fareed <88599145+KookiesNKareem@users.noreply.github.com>
2026-05-16 06:28:03 -0400

    Drop promoted scalar parameters from storage structs

--- KomaMRICore/src/simulation/SimMethods/Bloch/gpu/BlochGPU.jl
+++ KomaMRICore/src/simulation/SimMethods/Bloch/gpu/BlochGPU.jl
@@ -14,7 +14,7 @@ function prealloc(
     sim_method::SM, 
     backend::KA.GPU, 
     obj::Phantom{T}, 
-    M::Mag{T}, 
+    M::Mag,
     max_block_length::Integer, 
     groupsize
 ) where {T<:Real, SM<:BlochLikeSimMethods}
@@ -83,9 +83,9 @@ end
 
 function run_spin_precession!(
     p::Phantom{T},
-    seq::DiscreteSequence{T},
+    seq::DiscreteSequence,
     sig::AbstractArray{Complex{T}},
-    M::Mag{T},
+    M::Mag,
     sim_method::SM,
     groupsize::Integer,
     backend::KA.Backend,
@@ -164,9 +164,9 @@ end
 
 function run_spin_excitation!(
     p::Phantom{T},
-    seq::DiscreteSequence{T},
+    seq::DiscreteSequence,
     sig::AbstractArray{Complex{T}},
-    M::Mag{T},
+    M::Mag,
     sim_method::SM,
     groupsize::Integer,
     backend::KA.Backend,
