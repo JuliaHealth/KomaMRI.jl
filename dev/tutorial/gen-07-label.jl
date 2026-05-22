301be49513168baec03eb444794c47152d6fce7a
Documenter.jl <documenter@juliadocs.github.io>
2026-05-22 21:48:46 +0000

    build based on eae656a

--- dev/tutorial/gen-07-label.jl
+++ dev/tutorial/gen-07-label.jl
@@ -1,82 +1,64 @@
-using KomaMRI, PlotlyJS, Suppressor # hide
-sys = Scanner() # hide
-obj = brain_phantom3D()
-obj.Δw .= 0; # Removes the off-resonance
+using KomaMRI, PlotlyJS, Suppressor #hide
+sys = Scanner(); #hide
+Nx = 64
+Ny = 64
+Nslices = 2
+FOV = 23e-2 #hide
+Tadc = sys.ADC_Δt * (Nx - 1) #hide
+TR = 0.45 #hide
+slice_delay = 2.0 #hide
+TE = 5e-3 #hide
+Gx = 1 / (γ * sys.ADC_Δt * FOV) #hide
+Gz = 20e-3 #hide
+Trf = 2e-3 #hide
+slice_positions = range(-4e-3, 4e-3; length=Nslices); #hide
+obj = brain_phantom3D(); #hide
+obj.Δw .= 0; #hide
+phase_encode(lin) = Grad((lin - (Ny - 1) / 2) * Gx * sys.ADC_Δt / Tadc, Tadc) #hide
+rf = [PulseDesigner.RF_sinc(sys.B1, Trf, sys; G=[0, 0, Gz], Δf=γ * Gz * z) for z in slice_positions]; #hide
 
-seq_EPI = PulseDesigner.EPI_example()
-seq = deepcopy(seq_EPI);
-
-lSlcInc = LabelInc(1,"SLC");
-
-seq_EPI.EXT[1] = [lSlcInc];
-
-seq = seq + seq_EPI + seq_EPI
-plot_seq(seq);
-
-l = get_labels(seq);
-
-SLC_vec = [l[i].SLC for i in eachindex(l)];
-
-p1 = plot(SLC_vec, Layout(
-    xaxis_title="n° blocks",
-    yaxis_title="SLC label"
-))
-display(p1);
-
-sim_params = KomaMRICore.default_sim_params()
-raw = @suppress simulate(obj, seq, sys; sim_params)
-
-for p in [1,101,102,203]
-  slc = raw.profiles[p].head.idx.slice |> Int
-  println("Profile n°$p : SLC label = $slc")
+function cartesian_label_sequence()
+    seq = Sequence()
+    adc = ADC(Nx, Tadc)
+    readout = Grad(Gx, Tadc)
+    prewinder = Grad(-Gx / 2, Tadc)
+    @addblocks for slc in 0:(Nslices - 1), lin in 0:(Ny - 1)
+        pe = phase_encode(lin)
+        seq += rf[slc + 1]
+        seq += (x=prewinder, y=pe)
+        seq += Delay(TE) #hide
+        seq += (x=readout, adc, LabelSet(slc, "SLC"), LabelSet(lin, "LIN"))
+        seq += (y=-pe) #hide
+        seq += Delay(TR)
+        if lin == Ny - 1 #hide
+            seq += Delay(slice_delay) #hide
+        end #hide
+    end
+    return seq
 end
 
-acqData = AcquisitionData(raw)
-acqData.kdata |> size
+seq = cartesian_label_sequence();
 
-size_readout = size(acqData.traj[1].nodes,2) / 3 |> Int
-acqData.traj[1].nodes = acqData.traj[1].nodes[1:2,1:size_readout];
+raw = @suppress simulate(obj, seq, sys)
 
-C = maximum(2*abs.(acqData.traj[1].nodes[1:2]))
-acqData.traj[1].nodes ./= C
-acqData.traj[1].circular = false # Removing circular window
-
-Nx, Ny = raw.params["reconSize"][1:2]
-
-recParams = Dict{Symbol,Any}()
-recParams[:reconSize] = (Nx, Ny)
-recParams[:densityWeighting] = true
-rec = reconstruction(acqData, recParams)
-image = abs.(reshape(rec,Nx,Ny,:));
-
-p2 = plot_image(image[:,:,1], height=400);
-p3 = plot_image(image[:,:,2], height=400);
-display([p2 p3]);
-
-seq_LIN = PulseDesigner.EPI_example()
-lInc = LabelInc(1,"LIN");
-
-idx_ADC = is_ADC_on.(seq_LIN)
-for i in eachindex(idx_ADC)
-  idx_ADC[i] == 1 ? seq_LIN.EXT[i] = [lInc] : nothing;
+for p in [1, Ny, Ny + 1, Nslices * Ny]
+    slc = raw.profiles[p].head.idx.slice |> Int
+    lin = raw.profiles[p].head.idx.kspace_encode_step_1 |> Int
+    println("Profile $p: SLC=$slc, LIN=$lin")
 end
 
-seq_LIN.EXT[1] = [LabelSet(-1,"LIN")];
+raw.params["trajectory"] = "cartesian";
+raw.params["encodedSize"] = [Nx, Ny, 1];
+raw.params["reconSize"] = [Nx, Ny, 1];
 
-l = get_labels(seq_LIN)
-l[idx_ADC][1:10]
+acqData = AcquisitionData(raw; estimateProfileCenter=true);
+rec = reconstruction(acqData, Dict{Symbol,Any}());
+image = abs.(rec);
+image_display = sqrt.(image ./ maximum(image)); #hide
 
-raw = @suppress simulate(obj, seq_LIN, sys; sim_params)
-
-raw.params["trajectory"] = "cartesian"
-raw.params["encodedSize"] = [seq.DEF["Nx"],seq.DEF["Ny"]];
-
-acqData = AcquisitionData(raw,estimateProfileCenter=true);
-
-recParams = Dict{Symbol,Any}()
-rec = reconstruction(acqData, recParams);
-
-p4=plot_image(abs.(rec[:,:,1]), height=400)
-display(p4);
+p1 = plot_image(image_display[:, :, 1]; height=360, title="Slice 1", zmin=0, zmax=1)
+p2 = plot_image(image_display[:, :, 2]; height=360, title="Slice 2", zmin=0, zmax=1)
+foreach(p -> p.plot.data[1].showscale = false, (p1, p2)); #hide
+display([p1 p2]);
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
