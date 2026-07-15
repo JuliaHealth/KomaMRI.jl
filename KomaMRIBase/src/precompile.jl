48c95770cb25acec7cacf76a414f73abaccbafe4
anvika-singhal <anvikasinghal@outlook.com>
2026-07-15 15:08:36 -0700

    Add revisions

--- KomaMRIBase/src/precompile.jl
+++ KomaMRIBase/src/precompile.jl
@@ -5,34 +5,20 @@ import KomaMRIBase: PulseDesigner as PD
 
 @setup_workload begin
     @compile_workload begin
-        redirect_stderr(devnull) do
-            sys = Scanner()
-            obj = brain_phantom2D()
-            
-            seq = Sequence()
-            
-            PD.build_trapezoid(:x; area=8e-6u"T*s/m", sys)
-            PD.build_block_pulse(90u"deg"; duration=1u"ms", sys, use=Excitation())
-            PD.build_sinc_pulse(90u"deg"; duration=2u"ms", sys, use=Excitation())
-            PD.build_arbitrary_rf([1, 2, 1], 45u"deg"; dwell=200u"μs", sys, use=Excitation())
-            PD.build_adc(16; dwell=1u"μs", sys)
-            PD.build_label(:SET, :LIN, 3; sys)
-            PD.build_rotation(60u"deg"; sys)
-            PD.build_trigger(:physio1; sys)
-            PD.build_arbitrary_grad(:x, [0, 1, 0] .* u"mT/m"; sys)
-            
-            seq2 = Sequence()
-            append!(seq2, PD.build_trapezoid(:x; area=8e-6u"T*s/m", sys))
-            append!(seq2, PD.build_block_pulse(90u"deg"; duration=1u"ms", sys, use=Excitation()))
-            append!(seq2, PD.build_sinc_pulse(90u"deg"; duration=2u"ms", sys, use=Excitation()))
-            append!(seq2, PD.build_arbitrary_rf([1, 2, 1], 45u"deg"; dwell=200u"μs", sys, use=Excitation()))
-            append!(seq2, PD.build_adc(16; dwell=1u"μs", sys))
-            append!(seq2, PD.build_label(:SET, :LIN, 3; sys))
-            append!(seq2, PD.build_rotation(60u"deg"; sys))
-            append!(seq2, PD.build_trigger(:physio1; sys))
-            append!(seq2, PD.build_arbitrary_grad(:x, [0, 1, 0] .* u"mT/m"; sys))
-            
-            seq_disc = discretize(seq2, sys)
-        end
+        sys = Scanner()
+        obj = brain_phantom2D()
+        
+        seq = Sequence()
+        append!(seq, PD.build_trapezoid(:x; area=8e-6, sys))
+        append!(seq, PD.build_block_pulse(pi/2; duration=1e-3, sys, use=Excitation()))
+        append!(seq, PD.build_sinc_pulse(pi/2; duration=2e-3, sys, use=Excitation()))
+        append!(seq, PD.build_arbitrary_rf([1, 2, 1], pi/4; dwell=200e-6, sys, use=Excitation()))
+        append!(seq, PD.build_adc(16; dwell=1e-6, sys))
+        append!(seq, PD.build_label(:SET, :LIN, 3; sys))
+        append!(seq, PD.build_rotation(pi/3; sys))
+        append!(seq, PD.build_trigger(:physio1; sys))
+        append!(seq, PD.build_arbitrary_grad(:x, [0, 1, 0]; sys))
+        
+        seq_disc = discretize(seq, sys)
     end
 end
--- KomaMRICore/src/precompile.jl
+++ KomaMRICore/src/precompile.jl
@@ -5,62 +5,55 @@ import KomaMRIBase: PulseDesigner as PD
 
 @setup_workload begin
     @compile_workload begin
-        redirect_stderr(devnull) do
-            using KomaMRIBase
-            using KomaMRICore
-            
-            sys = Scanner()
-            obj_minimal = Phantom(
-                x=[0.0, 1e-3],
-                y=[0.0, 0.0],
-                z=[0.0, 0.0],
-                ρ=[1.0, 1.0],
-                T1=[0.8, 0.8],
-                T2=[80e-3, 80e-3],
-                T2s=[80e-3, 80e-3]
-            )
-            
-            seq = Sequence()
-            append!(seq, PD.build_trapezoid(:x; area=8e-6u"T*s/m", sys))
-            append!(seq, PD.build_block_pulse(90u"deg"; duration=1u"ms", sys, use=Excitation()))
-            append!(seq, PD.build_sinc_pulse(90u"deg"; duration=2u"ms", sys, use=Excitation()))
-            append!(seq, PD.build_arbitrary_rf([1, 2, 1], 45u"deg"; dwell=200u"μs", sys, use=Excitation()))
-            append!(seq, PD.build_adc(16; dwell=1u"μs", sys))
-            append!(seq, PD.build_label(:SET, :LIN, 3; sys))
-            append!(seq, PD.build_rotation(60u"deg"; sys))
-            append!(seq, PD.build_trigger(:physio1; sys))
-            append!(seq, PD.build_arbitrary_grad(:x, [0, 1, 0] .* u"mT/m"; sys))
-            
-            sim_methods = [
-                Bloch(),
-                BlochSimple(),
-                BlochDict(),
-                BlochMagnusConst1(),
-                BlochMagnusLin2(),
-                BlochMagnusMid2(),
-                BlochMagnusLinComm2(),
-                BlochMagnusQuad2(),
-                BlochMagnusQuad4(),
-                BlochMagnusGL2(),
-                BlochMagnusGL4(),
-                BlochMagnusBGL4(),
-                BlochMagnusBGL6(),
-            ]
-            
-            precisions = ["f32", "f64"]
-            return_types = ["mat", "raw"]
-            
-            for sim_method in sim_methods
-                for precision in precisions
-                    for return_type in return_types
-                        sim_params = Dict{String,Any}(
-                            "sim_method" => sim_method,
-                            "precision" => precision,
-                            "return_type" => return_type,
-                            "gpu" => false
-                        )
-                        signal = simulate(obj_minimal, seq, sys; sim_params)
-                    end
+        using KomaMRIBase
+        using KomaMRICore
+        
+        sys = Scanner()
+        obj_minimal = Phantom(
+            x=[0.0, 1e-3],
+            y=[0.0, 0.0],
+            z=[0.0, 0.0],
+            ρ=[1.0, 1.0],
+            T1=[0.8, 0.8],
+            T2=[80e-3, 80e-3],
+            T2s=[80e-3, 80e-3]
+        )
+        
+        seq = Sequence()
+        append!(seq, PD.build_trapezoid(:x; area=8e-6, sys))
+        append!(seq, PD.build_block_pulse(pi/2; duration=1e-3, sys, use=Excitation()))
+        append!(seq, PD.build_sinc_pulse(pi/2; duration=2e-3, sys, use=Excitation()))
+        append!(seq, PD.build_arbitrary_rf([1, 2, 1], pi/4; dwell=200e-6, sys, use=Excitation()))
+        append!(seq, PD.build_adc(16; dwell=1e-6, sys))
+        append!(seq, PD.build_label(:SET, :LIN, 3; sys))
+        append!(seq, PD.build_rotation(pi/3; sys))
+        append!(seq, PD.build_trigger(:physio1; sys))
+        append!(seq, PD.build_arbitrary_grad(:x, [0, 1, 0]; sys))
+        
+        sim_methods = [
+            Bloch(),
+            BlochSimple(),
+            BlochMagnusConst1(),
+            BlochMagnusLin2(),
+            BlochMagnusMid2(),
+            BlochMagnusQuad2(),
+            BlochMagnusQuad4(),
+            BlochMagnusGL4(),
+        ]
+        
+        precisions = ["f32", "f64"]
+        return_types = ["mat", "raw"]
+        
+        for sim_method in sim_methods
+            for precision in precisions
+                for return_type in return_types
+                    sim_params = Dict{String,Any}(
+                        "sim_method" => sim_method,
+                        "precision" => precision,
+                        "return_type" => return_type,
+                        "gpu" => false
+                    )
+                    signal = simulate(obj_minimal, seq, sys; sim_params, verbose=false)
                 end
             end
         end
