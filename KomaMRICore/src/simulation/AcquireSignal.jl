0ccbd6f4f9cb3a7fd3d6de864520b30efb949fa7
maple <90796818+aiyaszk@users.noreply.github.com>
2026-07-16 01:28:55 -0700

    Remove repeated receive sensitivity interpolation

--- KomaMRICore/src/simulation/AcquireSignal.jl
+++ KomaMRICore/src/simulation/AcquireSignal.jl
@@ -24,24 +24,12 @@ end
 
 # Arbitrary Bloch Simple run_spin_precession not sample based
 function acquire_signal!(sig, p, receiver::ArbitraryCoilSens, Mxy::AbstractMatrix)
-    interpolated_coil_sens = similar(receiver.coil_sens, length(p.x), size(receiver.coil_sens, 4))
-    for i in 1:size(receiver.coil_sens, 4)
-        base_itp = KomaMRIBase.GriddedInterpolation((receiver.x, receiver.y, receiver.z), receiver.coil_sens[:,:,:,i], KomaMRIBase.Gridded(KomaMRIBase.Linear()))
-        itp = KomaMRIBase.extrapolate(base_itp, 0f0)
-        interpolated_coil_sens[:,i] = itp.(p.x, p.y, p.z)
-        sig[:, i] .= vec(sum(interpolated_coil_sens[:, i] .* Mxy, dims=1))
-    end
+    sig .= transpose(Mxy) * get_sens(receiver, p.x, p.y, p.z)
     return nothing
 end
 
 # Arbitrary other sim methods
 function acquire_signal!(sig, p, receiver::ArbitraryCoilSens, Mxy::AbstractVector)
-    interpolated_coil_sens = similar(receiver.coil_sens, length(p.x), size(receiver.coil_sens, 4))
-    for i in 1:size(receiver.coil_sens, 4)
-        base_itp = KomaMRIBase.GriddedInterpolation((receiver.x, receiver.y, receiver.z), receiver.coil_sens[:,:,:,i], KomaMRIBase.Gridded(KomaMRIBase.Linear()))
-        itp = KomaMRIBase.extrapolate(base_itp, 0f0)
-        interpolated_coil_sens[:,i] = itp.(p.x, p.y, p.z)
-        sig[i] = sum(interpolated_coil_sens[:, i] .* Mxy)
-    end
+    sig .= vec(transpose(Mxy) * get_sens(receiver, p.x, p.y, p.z))
     return nothing
 end
